374fa7c053321b8ea3f533835782500d87421917
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-03-01 19:14:55 +0530

    bugs

--- resources/views/godpanel/client-form.blade.php
+++ resources/views/godpanel/client-form.blade.php
@@ -37,7 +37,8 @@
                         @csrf
                         <div class="row mb-2">
                             <div class="col-md-4"> <!--  Storage::disk('s3')->url($client->logo)  url('storage/'.$client->logo)--> 
-                                <input type="file" data-plugins="dropify" name="logo" data-default-file="{{isset($client->logo) ? Storage::disk('s3')->url($client->logo) : ''}}" />
+                                $urlImg = Auth::user()->logo['proxy_url'].'80/60'.Auth::user()->logo['image_path'];
+                                <input type="file" data-plugins="dropify" name="logo" data-default-file="{{isset($client->logo) ? $client->logo['proxy_url'].'80/60'.$client->logo['image_path'] : ''}}" />
                                 <p class="text-muted text-center mt-2 mb-0">Upload Logo</p>
                             </div>
                         </div>
