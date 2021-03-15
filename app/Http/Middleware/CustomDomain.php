5609102dc9d524edd90a431b59cd2631a2ad81b0
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-03-15 19:44:55 +0530

    multi domain

--- app/Http/Middleware/CustomDomain.php
+++ app/Http/Middleware/CustomDomain.php
@@ -45,7 +45,7 @@ class CustomDomain
         }
 
         $redisData = json_decode($existRedis);
-        dd($redisData->toArray());die;
+        dd($redisData);die;
 
         if($redisData){
             $database_name = 'royo_'.$redisData->database_name;
