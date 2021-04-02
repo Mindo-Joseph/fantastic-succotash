c24f591737522afd0f17508881b597228221c80a
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-04-02 17:29:53 +0530

    royo order

--- app/Http/Controllers/Front/FacebookController.php
+++ app/Http/Controllers/Front/FacebookController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Front\FrontController;
-use App\Models\{Currency, Category, Product, ClientLanguage, ClientCurrency, User, Country};
+use App\Models\{ClientLanguage, ClientCurrency, User, Country, UserDevice, UserVerification};
 use Illuminate\Http\Request;
 use Session;
 use Carbon\Carbon;
+use Config;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Two\FacebookProvider;
+use Laravel\Socialite\SocialiteManager;
 use Socialite;
 
 class FacebookController extends FrontController
@@ -19,9 +22,34 @@ class FacebookController extends FrontController
      *
      * @return void
      */
+    protected function createFacebookDriver()
+    {
+        $config = $this->config->get('services.facebook');
+        dd($config);
+
+        return $this->buildProvider(
+            FacebookProvider::class, $config
+        );
+    }
 
     public function redirectToFacebook()
     {
+        /*echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
+        echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
+        echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
+        echo $a = Config::get('FACEBOOK_CLIENT_ID'); echo ' - ';
+        echo $a = Config::get('FACEBOOK_CLIENT_ID'); echo ' - ';
+        echo $a = Config::get('FACEBOOK_CLIENT_ID');die;*/
+        /*$facebook2 = [
+            'client_id' => env('FACEBOOK_CLIENT_ID'),
+            'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+            'redirect' => env('FACEBOOK_CALLBACK_URL'),
+        ];*/
+        
+        $config = $this->config->get('services.facebook');
+        dd($config);
+
+
         return Socialite::driver('facebook')->redirect();
     }
 
@@ -30,37 +58,57 @@ class FacebookController extends FrontController
      *
      * @return void
      */
-
     public function handleFacebookCallback()
     {
+        /*$facebook2 = [
+            'client_id' => env('FACEBOOK_CLIENT_ID'),
+            'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+            'redirect' => env('FACEBOOK_CALLBACK_URL'),
+        ];*/
         try {
             $user = Socialite::driver('facebook')->user();
+            $user_name = $user->getName();
+            $user_email = $user->getEmail();
+            $facebook_auth_id = $user->getId();
+            /* other code */
 
-            echo '<pre>'; print_r($user); echo '</pre>';
+            $user = User::where('facebook_auth_id', $user->getId())->first();
+            if($user){
+                Auth::login($user);
+                return redirect()->route('userHome');
+            }
 
-            dd($user);
-            $create['name'] = $user->getName();
-            $create['email'] = $user->getEmail();
-            $create['facebook_id'] = $user->getId();
+            $user = new User();
 
-            $userModel = new User;
+            $user->name = $user->getName();
+            $user->email = $user->getEmail();
+            $user->facebook_auth_id = $user->getId();
+            $user->password = Hash::make($user->getId());
+            $user->type = 1;
+            $user->status = 1;
+            $user->role_id = 1;
+            $user->save();
 
-            $createdUser = $userModel->addNew($create);
+            if($user->id > 0){
+                $user_device[] = [
+                    'user_id' => $user->id,
+                    'device_type' => 'web',
+                    'device_token' => 'web',
+                    'access_token' => ''
+                ];
+                UserDevice::insert($user_device);
 
-            Auth::loginUsingId($createdUser->id);
-
-
-            return redirect()->route('home');
-
-
+                $user_verify[] = [
+                    'user_id' => $user->id,
+                    'is_email_verified' => 0,
+                    'is_phone_verified' => 0
+                ];
+                UserVerification::insert($user_verify);
+                Auth::login($user);
+                return redirect()->route('userHome');
+            }
         } catch (Exception $e) {
-
-
-            return redirect('auth/facebook');
-
-
+            return redirect()->route('userHome')->with(['error' => "facebook login failed."]);
         }
-
     }
-
 }
--- app/Http/Middleware/CustomDomain.php
+++ app/Http/Middleware/CustomDomain.php
@@ -28,8 +28,11 @@ class CustomDomain
       $subDomain = explode('.', $domain);
       $existRedis = Redis::get($domain);
 
+      //$callback = http://local.myorder.com/auth/facebook/callback
+      
+
       if(!$existRedis){
-        $client = Client::select('name', 'email', 'phone_number', 'is_deleted', 'is_blocked', 'logo', 'company_name', 'company_address', 'status', 'code', 'database_name', 'database_host', 'database_port', 'database_username', 'database_password')
+        $client = Client::select('name', 'email', 'phone_number', 'is_deleted', 'is_blocked', 'logo', 'company_name', 'company_address', 'status', 'code', 'database_name', 'database_host', 'database_port', 'database_username', 'database_password', 'custom_domain', 'sub_domain')
                     ->where(function($q) use($domain, $subDomain){
                               $q->where('custom_domain', $domain)
                                 ->orWhere('sub_domain', $subDomain[0]);
@@ -41,6 +44,7 @@ class CustomDomain
 
         $existRedis = Redis::get($domain);
       }
+      $callback = '';
 
       $redisData = json_decode($existRedis);
       //echo '<pre>';print_r($redisData);
@@ -70,11 +74,26 @@ class CustomDomain
           DB::setDefaultConnection($database_name);
           DB::purge($database_name);
 
+          if(!empty($redisData->custom_domain)){
+            $domain = rtrim($redisData->custom_domain, "/");
+            $domain = ltrim($domain, "https://");
+            $callback = "https://".$domain.'/auth/facebook/callback';
+          }else{
+            $sub_domain = rtrim($redisData->sub_domain, "/");
+            $sub_domain = ltrim($sub_domain, "https://");
+            $callback = "https://".$sub_domain.".royoorders.com/auth/facebook/callback";
+          }
+
           /*session()->forget('session_lang_id');
           session()->forget('session_cur_id');
           session()->flush();*/
 
           $clientPreference = ClientPreference::select('theme_admin', 'distance_unit', 'currency_id', 'date_format', 'time_format', 'fb_login', 'fb_client_id', 'fb_client_secret', 'fb_client_url', 'twitter_login', 'twitter_client_id', 'twitter_client_secret', 'twitter_client_url', 'google_login', 'google_client_id', 'google_client_secret', 'google_client_url', 'apple_login', 'apple_client_id', 'apple_client_secret', 'apple_client_url', 'Default_location_name', 'Default_latitude', 'Default_longitude', 'map_provider', 'map_key', 'sms_provider', 'verify_email', 'verify_phone', 'web_template_id', 'is_hyperlocal', 'need_delivery_service', 'need_dispacher_ride', 'delivery_service_key', 'dispatcher_key', 'primary_color', 'secondary_color')->where('client_code', $redisData->code)->first();
+
+          Config::set('FACEBOOK_CLIENT_ID', $clientPreference->fb_client_id);
+          Config::set('FACEBOOK_CLIENT_SECRET', $clientPreference->fb_client_secret);
+          Config::set('FACEBOOK_CALLBACK_URL', $callback);
+
 
           Session::put('client_config', $redisData);
           Session::put('login_user_type', 'client');
