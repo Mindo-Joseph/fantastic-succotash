8072fd2389288e3714212f7e6c7450caf8440e02
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-04-06 12:49:40 +0530

    facebook auth

--- app/Http/Controllers/Front/FacebookController.php
+++ app/Http/Controllers/Front/FacebookController.php
@@ -13,95 +13,32 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\FacebookProvider;
 use Laravel\Socialite\SocialiteManager;
 use Socialite;
 
 class FacebookController extends FrontController
 {
     private $conp;
-    public $scopeSeparator = ',';
-    public $parameters = [];
-    /**
-     * Get the GET parameters for the code request.
-     *
-     * @param  string|null  $state
-     * @return array
-     */
-    protected function getCodeFields($state = null)
-    {
-        echo "asdasdasd";die;
-        $fields = [
-            'client_id' => 'qewqweqeweeeqwewq',
-            'redirect_uri' => 'wqeeeeeeeeeeeeeeeeeeeqewqewqe',
-            'scope' => $this->formatScopes(AbstractProvider::getScopes(), $this->scopeSeparator),
-            'response_type' => 'code',
-        ];
 
-        if ($this->usesState()) {
-            $fields['state'] = $state;
-        }
+    private function makeFacebookDriver($domain = ''){
 
-        if ($this->usesPKCE()) {
-            $fields['code_challenge'] = $this->getCodeChallenge();
-            $fields['code_challenge_method'] = $this->getCodeChallengeMethod();
-        }
-
-        return array_merge($fields, $this->parameters);
+        $config['client_id'] = 'aaaaaaaaaa';
+        $config['client_secret'] = 'aaaaaaaaaaaaaaaaaa';
+        $config['redirect'] = 'http://'.$domain.'/auth/facebook/callback';
+        return  Socialite::buildProvider(FacebookProvider::class, $config);
     }
+    
 
-    /**
-     * Generates the PKCE code challenge based on the PKCE code verifier in the session.
-     *
-     * @return string
-     */
-    protected function getCodeChallenge()
-    {
-        $hashed = hash('sha256', $this->request->session()->get('code_verifier'), true);
-
-        return rtrim(strtr(base64_encode($hashed), '+/', '-_'), '=');
-    }
-
-    /**
-     * Returns the hash method used to calculate the PKCE code challenge.
-     *
-     * @return string
-     */
-    protected function getCodeChallengeMethod()
-    {
-        return 'S256';
-    }
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function redirectToFacebook(Request $request)
     {
-        $provider = new AbstractProvider($request, 'clientId', 'clientSecret', 'clientId', $guzzle = []);
-        /*echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
-        echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
-        echo $a = env('FACEBOOK_CLIENT_ID'); echo ' - ';
-        echo $a = Config::get('FACEBOOK_CLIENT_ID'); echo ' - ';
-        echo $a = Config::get('FACEBOOK_CLIENT_ID'); echo ' - ';
-        echo $a = Config::get('FACEBOOK_CLIENT_ID');die;*/
-        /*$facebook2 = [
-            'client_id' => env('FACEBOOK_CLIENT_ID'),
-            'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-            'redirect' => env('FACEBOOK_CALLBACK_URL'),
-        ];*/
-        $driver = Socialite::driver('facebook');
-  
-        //dd($driver->clientId); echo "  --   ";
+        //$driver = Socialite::driver('facebook');
+        //return Socialite::driver('facebook')->redirect();
+        $fb = $this->makeFacebookDriver('www.facebook.com');
+
+        dd($fb);
+        return $fb->redirect();
         
-        //echo '<pre>';print_r($driver->toArray()); echo '</pre>';
-        dd($driver);die;
-        return Socialite::driver('facebook')->redirect();
-        /*dd($aaa);
-        echo '1';
-        dd($this->conp);
-        return $aaa;*/
     }
 
     /**
