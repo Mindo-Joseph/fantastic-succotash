3ab929ddaba9c5f17542db87850880ebc62a109c
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-04-06 16:31:00 +0530

    facebook auth

--- app/Http/Controllers/Front/FacebookController.php
+++ app/Http/Controllers/Front/FacebookController.php
@@ -17,45 +17,58 @@ use Laravel\Socialite\Two\FacebookProvider;
 use Laravel\Socialite\Two\GoogleProvider;
 use Laravel\Socialite\One\TwitterProvider;
 use Laravel\Socialite\SocialiteManager;
+use League\OAuth1\Client\Server\Twitter as TwitterServer;
 use Socialite;
 
 class FacebookController extends FrontController
 {
-    private $conp;
-
-    private function makeSocialDriver($domain = '', $driver = 'facebook'){
+    private function configDriver($domain = '', $driver = 'facebook'){
         $ClientPreferences = ClientPreference::select('fb_login', 'fb_client_id', 'fb_client_secret', 'fb_client_url', 'twitter_login', 'twitter_client_id', 'twitter_client_secret', 'twitter_client_url', 'google_login', 'google_client_id', 'google_client_secret', 'google_client_url', 'apple_login', 'apple_client_id', 'apple_client_secret', 'apple_client_url')->first();
 
-        //echo $ss = session('preferences')->fb_client_id ; echo ' == ';
-        //echo $ss = session('preferences')->fb_client_secret;
-        //$driver;
+       
         if($driver == 'facebook'){
             $config['client_id'] = $ClientPreferences->fb_client_id;
             $config['client_secret'] = $ClientPreferences->fb_client_secret;
-            $config['redirect'] = 'https://'.$domain.'/auth/callback/facebook';die;
+            $config['redirect'] = 'https://'.$domain.'/auth/callback/facebook';
 
-            return  Socialite::buildProvider(FacebookProvider::class, $config);
+            return Socialite::buildProvider(FacebookProvider::class, $config);
 
-        } elseif ($driver == 'twitter'){
-            $config['client_id'] = $ClientPreferences->twitter_client_id;
-            $config['client_secret'] = $ClientPreferences->twitter_client_secret;
-            $config['redirect'] = 'https://'.$domain.'/auth/callback/twitter';
-
-            return  Socialite::buildProvider(TwitterProvider::class, $config);
-            
         } elseif ($driver == 'google'){
             $config['client_id'] = $ClientPreferences->google_client_id;
             $config['client_secret'] = $ClientPreferences->google_client_secret;
             $config['redirect'] = 'https://'.$domain.'/auth/callback/google';
 
-            return  Socialite::buildProvider(GoogleProvider::class, $config);
+            return Socialite::buildProvider(GoogleProvider::class, $config);
             
+        }elseif ($driver == 'twitter'){
+            $config['client_id'] = $ClientPreferences->twitter_client_id;
+            $config['client_secret'] = $ClientPreferences->twitter_client_secret;
+            $config['redirect'] = 'https://'.$domain.'/auth/callback/twitter';
+
+            $array_merge = array_merge([
+                'identifier' => $config['client_id'],
+                'secret' => $config['client_secret'],
+                'callback_uri' => $this->formatRedirectUrl($config),
+            ], $config);
+
+            return new TwitterProvider(
+                $this->container->make('request'), new TwitterServer($array_merge)
+            );            
         }
+    }
+
+    private function formatRedirectUrl(array $config)
+    {
+        $redirect = value($config['redirect']);
+
+        return Str::startsWith($redirect, '/')
+                    ? $this->container->make('url')->to($redirect)
+                    : $redirect;
     }
 
     public function redirectToSocial($domain = '', $redirecting = 'facebook')
     {
-        $fb = $this->makeSocialDriver($domain, $redirecting);
+        $fb = $this->configDriver($domain, $redirecting);
 
         return $fb->redirect();
     }
