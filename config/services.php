32c1894212a0d25c925cfb9effab2da326082019
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-04-07 19:21:59 +0530

    apple login

--- config/services.php
+++ config/services.php
@@ -51,7 +51,7 @@ return [
     'apple' => [
         'client_id' => "com.royoorders-apple-login",
         'client_secret' => "eyJraWQiOiIzRjhVQTZDN1ozIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiI2RlhVTVQ1WDIzIiwiYXVkIjoiaHR0cHM6Ly9hcHBsZWlkLmFwcGxlLmNvbSIsInN1YiI6ImNvbS5yb3lvb3JkZXJzLWFwcGxlLWxvZ2luIiwiaWF0IjoxNjE3Nzk0ODcyLCJleHAiOjE2MzMzNDY4NzJ9.KXuDGrLgwKaWSuFfiHlbjZ4aaA4AZnlgI0jT7EZMXBya7NkdDbIvCUD6LnRZGT_KtgfR3YM0gEYszxyGRgNS8A",
-        'redirect' => "http://local.myorder.com/auth/apple/callback",
+        'redirect' => "https://bahubali.royoorders.com/auth/callback/apple",
     ],
 
 
