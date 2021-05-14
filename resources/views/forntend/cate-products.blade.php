ef8d4496270200c81026954772b281991e02fe21
pankaj rana <mail2ppundir@gmail.com>
2021-05-14 18:34:23 +0530

    changes in cat products page

--- resources/views/forntend/cate-products.blade.php
+++ resources/views/forntend/cate-products.blade.php
@@ -222,58 +222,57 @@
                                         </div>
                                     </div>
                                     <div class="displayProducts">
-                                        <div class="product-wrapper-grid">
-                                            <div class="row margin-res">
+                                        @if(!empty($listData))
+                                            <div class="product-wrapper-grid">
+                                                <div class="row margin-res">
+                                                    @foreach($listData as $key => $data)
 
-                                              @if(!empty($listData))
-                                                @foreach($listData as $key => $data)
-
-                                                <?php $imagePath = $imagePath2 = '';
-                                                $mediaCount = count($data->media);
-                                                for ($i = 0; $i < $mediaCount && $i < 2; $i++) { 
-                                                    if($i == 0){
-                                                        $imagePath = $data->media[$i]->image->path['proxy_url'].'300/300'.$data->media[$i]->image->path['image_path'];
-                                                    }
-                                                    $imagePath2 = $data->media[$i]->image->path['proxy_url'].'300/300'.$data->media[$i]->image->path['image_path'];
-                                                } ?>
-                                                <div class="col-xl-3 col-6 col-grid-box">
-                                                    <div class="product-box">
-                                                        <div class="img-wrapper">
-                                                            <div class="front">
-                                                                <a href="{{route('productDetail', $data->sku)}}"><img class="img-fluid blur-up lazyload" src="{{$imagePath}}" alt=""></a>
+                                                    <?php $imagePath = $imagePath2 = '';
+                                                    $mediaCount = count($data->media);
+                                                    for ($i = 0; $i < $mediaCount && $i < 2; $i++) { 
+                                                        if($i == 0){
+                                                            $imagePath = $data->media[$i]->image->path['proxy_url'].'300/300'.$data->media[$i]->image->path['image_path'];
+                                                        }
+                                                        $imagePath2 = $data->media[$i]->image->path['proxy_url'].'300/300'.$data->media[$i]->image->path['image_path'];
+                                                    } ?>
+                                                    <div class="col-xl-3 col-6 col-grid-box">
+                                                        <div class="product-box">
+                                                            <div class="img-wrapper">
+                                                                <div class="front">
+                                                                    <a href="{{route('productDetail', $data->sku)}}"><img class="img-fluid blur-up lazyload" src="{{$imagePath}}" alt=""></a>
+                                                                </div>
+                                                                <div class="back">
+                                                                    <a href="{{route('productDetail', $data->sku)}}"><img class="img-fluid blur-up lazyload" src="{{$imagePath2}}" alt=""></a>
+                                                                </div>
+                                                                <div class="cart-info cart-wrap">
+                                                                    <button data-toggle="modal" data-target="#addtocart" title="Add to cart"><i class="ti-shopping-cart"></i></button> 
+                                                                    <a href="javascript:void(0)" title="Add to Wishlist" class="addWishList" proSku="{{$data->sku}}"><i class="ti-heart" aria-hidden="true"></i></a>
+                                                                    <!-- <a data-toggle="modal" href="#" data-target="#quick-view" title="Quick View"><i class="ti-search" aria-hidden="true"></i></a>
+                                                                    <a href="compare.html" title="Compare"><i class="ti-reload" aria-hidden="true"></i></a> -->
+                                                                </div>
                                                             </div>
-                                                            <div class="back">
-                                                                <a href="{{route('productDetail', $data->sku)}}"><img class="img-fluid blur-up lazyload" src="{{$imagePath2}}" alt=""></a>
-                                                            </div>
-                                                            <div class="cart-info cart-wrap">
-                                                                <button data-toggle="modal" data-target="#addtocart" title="Add to cart"><i class="ti-shopping-cart"></i></button> 
-                                                                <a href="javascript:void(0)" title="Add to Wishlist" class="addWishList" proSku="{{$data->sku}}"><i class="ti-heart" aria-hidden="true"></i></a>
-                                                                <!-- <a data-toggle="modal" href="#" data-target="#quick-view" title="Quick View"><i class="ti-search" aria-hidden="true"></i></a>
-                                                                <a href="compare.html" title="Compare"><i class="ti-reload" aria-hidden="true"></i></a> -->
-                                                            </div>
-                                                        </div>
-                                                        <div class="product-detail">
-                                                            <div>
-                                                                <div class="rating">
-                                                                @for($i = 1; $i < 6; $i++)
-                                                                    <i class="fa fa-star"></i>
-                                                                @endfor
-                                                            </div>
-                                                            <a href="{{route('productDetail', $data->sku)}}">
-                                                                <h6>{{(!empty($data->translation) && isset($data->translation[0])) ? $data->translation[0]->title : ''}}</h6>
-                                                            </a>
-                                                            <h4>{{Session::get('currencySymbol').($data->variant[0]->price * $data->variant[0]->multiplier)}}</h4>
+                                                            <div class="product-detail">
+                                                                <div>
+                                                                    <div class="rating">
+                                                                    @for($i = 1; $i < 6; $i++)
+                                                                        <i class="fa fa-star"></i>
+                                                                    @endfor
+                                                                </div>
+                                                                <a href="{{route('productDetail', $data->sku)}}">
+                                                                    <h6>{{(!empty($data->translation) && isset($data->translation[0])) ? $data->translation[0]->title : ''}}</h6>
+                                                                </a>
+                                                                <h4>{{Session::get('currencySymbol').($data->variant[0]->price * $data->variant[0]->multiplier)}}</h4>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
+                                                    @endforeach
                                                 </div>
-                                                @endforeach
-                                              @endif
                                             </div>
-                                        </div>
-                                        <div class="pagination pagination-rounded justify-content-end mb-0">
-                                            {{ $listData->links() }}
-                                        </div>
+                                            <div class="pagination pagination-rounded justify-content-end mb-0">
+                                                {{ $listData->links() }}
+                                            </div>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
@@ -284,11 +283,8 @@
         </div>
     </div>
 </section>
-
 @endsection
-
 @section('script')
-
 <script src="{{asset('front-assets/js/rangeSlider.min.js')}}"></script>
 <script src="{{asset('front-assets/js/my-sliders.js')}}"></script>
 <script>
