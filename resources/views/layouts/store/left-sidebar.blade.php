0079f21f1aee9934a97940dd96045b918af793cb
Yogendra Kaushik <yogendra.k@codebrewinnovations.com>
2021-05-14 15:51:58 +0530

    logo

--- resources/views/layouts/store/left-sidebar.blade.php
+++ resources/views/layouts/store/left-sidebar.blade.php
@@ -1,5 +1,6 @@
 @php
 $clientData = \App\Models\Client::select('id', 'logo')->where('id', '>', 0)->first();
+$urlImg = $clientData->logo['proxy_url'].'200/80/'.$clientData->logo['image_path'];
 $languageList = \App\Models\ClientLanguage::with('language')->where('is_active', 1)->orderBy('is_primary', 'desc')->get();
 $currencyList = \App\Models\ClientCurrency::with('currency')->orderBy('is_primary', 'desc')->get();
 @endphp
@@ -55,7 +56,7 @@ $currencyList = \App\Models\ClientCurrency::with('currency')->orderBy('is_primar
                         </div>
                     </div>
                     <div class="brand-logo">
-                        <a href="{{ route('userHome') }}"><img src="{{$clientData->logo['proxy_url'] . '120/100' . $clientData->logo['image_path']}}" class="img-fluid blur-up lazyload" alt=""></a>
+                        <a href="{{ route('userHome') }}"><img src="{{$urlImg}}" class="img-fluid blur-up lazyload" alt=""></a>
                     </div>
                 </div>
                 <div class="menu-right pull-right">
