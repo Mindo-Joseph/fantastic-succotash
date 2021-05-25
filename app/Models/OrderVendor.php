f2cce550fe13642765ab8d2c96837e324669c2e1
Suneel153 <suneel.1@codebrewinnovations.com>
2021-05-25 17:42:44 +0530

    changes in order api

--- app/Models/OrderVendor.php
+++ app/Models/OrderVendor.php
@@ -10,10 +10,10 @@ class OrderVendor extends Model{
     public function vendor(){
 	    return $this->hasOne('App\Models\Vendor' , 'id', 'vendor_id'); 
 	}
-    public function products(){
-	    return $this->hasMany('App\Models\OrderProduct' , 'vendor_id', 'id'); 
-	}
 	
+    public function products(){
+	    return $this->hasMany('App\Models\OrderProduct' , 'vendor_id', 'vendor_id'); 
+	}
 	public function coupon(){
 	    return $this->hasOne('App\Models\Promocode' , 'id', 'coupon_id'); 
 	}
