43e995b81ad87a2ffe9f2930ea6ff77ea30f9c00
chander_codebrew <chandar@codebrewinnovations.com>
2021-05-26 11:55:41 +0530

    new changes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.store', ['title' => 'Product'])
 @section('content')
     <section class="wrapper-main">
-        <div class="container"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mb-lg-0 mb-3">
+                    <h3>Login to Your Account</h3>
+                    <div class="sub_heading">Social media logins</div>
+                    <ul class="d-flex align-items-center m-auto">
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6"></div
+            </div>
+        </div>
     </section> 
 @endsection
