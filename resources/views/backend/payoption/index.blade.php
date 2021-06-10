5c2ccb4ca127274d8529d6bca7e94331689b3dde
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-06-10 15:36:29 +0530

    merge data

--- resources/views/backend/payoption/index.blade.php
+++ resources/views/backend/payoption/index.blade.php
@@ -34,16 +34,62 @@
                 <input type="hidden" name="method_name" id="{{$opt->title}}" value="{{$opt->title}}">
                 @csrf
                 @method('PUT')
+
+                <?php 
+                $creds = json_decode($opt->credentials);
+                $username = (isset($creds->username)) ? $creds->username : '';
+                $password = (isset($creds->password)) ? $creds->password : '';
+                $signature = (isset($creds->signature)) ? $creds->signature : '';
+                $api_key = (isset($creds->api_key)) ? $creds->api_key : '';
+                ?>
+
                 <div class="card-box">
                     <div class="d-flex align-items-center justify-content-between mb-2">
                         <h4 class="header-title text-uppercase mb-0">{{$opt->title}}</h4>
-                        
-                        <span class="d-block">
-                            <input type="checkbox" data-id="{{$opt->id}}" data-plugin="switchery" name="active" class="chk_box all_select" data-color="#43bee1" @if($opt->status == 1) checked @endif>
-                        </span>
-                       
+                        <button class="btn btn-info btn-block save_btn" type="submit"> Save </button>
                     </div>
-                    
+                    <div class="form-group mb-0 switchery-demo">
+                        <label for="" class="mr-3">Enable</label>
+                        <input type="checkbox" data-id="{{$opt->id}}" data-title="{{$opt->title}}" data-plugin="switchery" name="active" class="chk_box all_select" data-color="#43bee1" @if($opt->status == 1) checked @endif>
+                    </div>
+
+                    @if ( (strtolower($opt->title) == 'stripe') )
+                    <div id="stripe_fields_wrapper" @if($opt->status != 1) style="display:none" @endif>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group mb-0">
+                                    <label for="stripe_api_key" class="mr-3">API Key</label>
+                                    <input type="textbox" name="stripe_api_key" id="stripe_api_key" class="form-control" value="{{$api_key}}" @if($opt->status == 1) required @endif>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+
+                    @if ( (strtolower($opt->title) == 'paypal') )
+                    <div id="paypal_fields_wrapper" @if($opt->status != 1) style="display:none" @endif>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group mb-0">
+                                    <label for="paypal_username" class="mr-3">Username</label>
+                                    <input type="textbox" name="paypal_username" id="paypal_username" class="form-control" value="{{$username}}" @if($opt->status == 1) value="" required @endif>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group mb-0">
+                                    <label for="paypal_password" class="mr-3">Password</label>
+                                    <input type="textbox" name="paypal_password" id="paypal_password" class="form-control" value="{{$password}}" @if($opt->status == 1) required @endif>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group mb-0">
+                                    <label for="paypal_signature" class="mr-3">Signature</label>
+                                    <input type="textbox" name="paypal_signature" id="paypal_signature" class="form-control" value="{{$signature}}" @if($opt->status == 1) required @endif>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                     <!-- <div class="d-flex align-items-center justify-content-between mb-2">
                         <button class="btn btn-info d-block" type="submit"> Save </button>
                     </div> -->
@@ -67,8 +113,32 @@
 
         $('.all_select').change(function(){
             var id = $(this).data('id');
-            console.log(id);
-            $('#form_'+id).submit();
+            // console.log(id);
+            var title = $(this).data('title');
+
+            if( title.toLowerCase() == 'stripe' ){
+                if($(this).is(":checked")){
+                    $("#stripe_fields_wrapper").show();
+                    $("#stripe_fields_wrapper").find('input').attr('required', true);
+                }
+                else{
+                    $("#stripe_fields_wrapper").hide();
+                    $("#stripe_fields_wrapper").find('input').removeAttr('required');
+                }
+            }
+
+            if( title.toLowerCase() == 'paypal' ){
+                if($(this).is(":checked")){
+                    $("#paypal_fields_wrapper").show();
+                    $("#paypal_fields_wrapper").find('input').attr('required', true);
+                }
+                else{
+                    $("#paypal_fields_wrapper").hide();
+                    $("#paypal_fields_wrapper").find('input').removeAttr('required');
+                }
+            }
+
+            // $('#form_'+id).submit();
 
             //$('.vendorRow').toggle();
         });
