073769539fb25bdf1fae087eaa31e4ed3bfda8ce
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-10 16:14:56 +0530

    changes in client dashboard controller

--- resources/views/backend/vendor/vendorCatalog.blade.php
+++ resources/views/backend/vendor/vendorCatalog.blade.php
@@ -122,7 +122,7 @@
                                                     @endif
                                                   </td>
                                                   <td> {{ (isset($product->primary->title) && !empty($product->primary->title)) ? $product->primary->title : '' }} </td>
-                                                  <td> {{ $product->category->cat->slug }}</td>
+                                                  <td> {{ $product->category ? $product->category->cat->slug : '' }}</td>
                                                   <td> {{ !empty($product->brand) ? $product->brand->title : 'N/A'  }}</td>
                                                   <td> {{ $product->variant->first() ? $product->variant->first()->quantity : 0 }}</td>
                                                   <td> {{ $product->variant->first() ? $product->variant->first()->price : 0 }}</td>
