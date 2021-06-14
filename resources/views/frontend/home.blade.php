511c3cbfda103c49f211bfa1fd0dbb1021e7b4dc
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-14 19:26:03 +0530

    changes in import vendor via csv

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.store', ['title' => 'Home'])
-
 @section('css')
 <style type="text/css">
     .main-menu .brand-logo {
@@ -8,16 +7,12 @@
         padding-bottom: 20px;
     }
 </style>
-
 @endsection
-
 @section('content')
-
 <header>
     <div class="mobile-fix-option"></div>
     @include('layouts.store/left-sidebar')
 </header>
-
 <div class="find_location">
     <div class="container px-0">
         <div class="row no-gutters">
@@ -41,86 +36,17 @@
         </div>
     </div>
 </div>
-
 <section class="p-0 small-slider">
     <div class="slide-1 home-slider">
         @foreach($banners as $banner)
         <div>
             <div class="home  text-center">
-                <img src="{{$banner->image['proxy_url'] . '1500/600' . $banner->image['image_path']}}"
-                    class="bg-img blur-up lazyload" alt="">
-                    <!--<div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="slider-contain">
-                                    <div>
-                                        <h4>welcome to fashion</h4>
-                                        <h1>men fashion</h1>
-                                        <a href="#" class="btn btn-solid">shop now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
+                <img src="{{$banner->image['proxy_url'] . '1500/600' . $banner->image['image_path']}}" class="bg-img blur-up lazyload" alt="">
             </div>
         </div>
         @endforeach
     </div>
 </section>
-
-
-
-<!-- Home slider end -->
-<!-- category -->
-<!-- <section class="section-b-space border-section border-top-0">
-        <div class="row">
-            <div class="col">
-                <div class="slide-6 no-arrow">
-                    @foreach($navCategories as $cate)
-                    <div class="category-block">
-                        <a href="{{route('categoryDetail', $cate['id'])}}">
-                            <div class="category-image"><img src="{{$cate['icon']['proxy_url'].'40/30'.$cate['icon']['image_path']}}" alt=""></div>
-                        </a>
-                        <div class="category-details">
-                            <a href="{{route('categoryDetail', $cate['id'])}}">
-                                <h5>{{$cate['name']}}</h5>
-                            </a>
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </section> -->
-
-<!-- Paragraph-->
-<!-- <div class="title1 section-t-space">
-        <h2 class="title-inner1">Vendors</h2>
-    </div> -->
-<!-- Home Banner Start From Here -->
-<!-- <section class="home-banner">
-    <div class="home_slider">
-        <div>
-            <div class="main-img" style="background: url({{asset('assets/images/food.webp')}});">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="offset-lg-6"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <div class="main-img" style="background: url({{asset('assets/images/food.webp')}});">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="offset-lg-6"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-
 <section class="section-b-space p-t-0 pt-5 ratio_asos">
     <div class="container">
         <div class="row">
@@ -139,22 +65,7 @@
                                         class="img-fluid blur-up lazyload bg-img" alt=""
                                         src="{{$vendor->logo['proxy_url'] . '300/300' . $vendor->logo['image_path']}}"></a>
                             </div>
-                            <!-- <div class="cart-info cart-wrap">
-                                    <button data-toggle="modal" data-target="#addtocart" title="Add to cart">
-                                        <i class="ti-shopping-cart"></i>
-                                    </button>
-                                    <a href1="javascript:void(0)" title="Add to Wishlist">
-                                        <i class="ti-heart" aria-hidden="true"></i>
-                                    </a>
-                                    <a href1="#" data-toggle="modal" data-target="#quick-view" title="Quick View">
-                                        <i class="ti-search" aria-hidden="true"></i>
-                                    </a>
-                                    <a href1="compare.html" title="Compare">
-                                        <i class="ti-reload" aria-hidden="true"></i>
-                                    </a>
-                                </div> -->
                         </div>
-
                         <div class="product-detail">
                             <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
                                     class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
@@ -162,23 +73,14 @@
                             <a href="#">
                                 <h6>{{$vendor->name}}</h6>
                             </a>
-                            <!-- <h4>{{$vendor->name}}</h4>
-                                <ul class="color-variant">
-                                    <li class="bg-light0"></li>
-                                    <li class="bg-light1"></li>
-                                    <li class="bg-light2"></li>
-                                </ul> -->
                         </div>
                     </div>
                     @endforeach
-
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- Product slider end -->
-
 <section class="section-b-space">
     <div class="container">
         <div class="row multiple-slider">
@@ -344,42 +246,6 @@
                         </div>
                     </div>
                     @endforeach
-
-                    <!--<div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/2.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/3.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/4.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/5.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/6.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/7.png')}}" alt=""></a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="logo-block">
-                                <a href="#"><img src="{{asset('front-assets/images/logos/8.png')}}" alt=""></a>
-                            </div>
-                        </div> -->
                 </div>
             </div>
         </div>
