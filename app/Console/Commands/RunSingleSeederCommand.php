0010f66c525f888539116f7a8bb08ef02d5e5e32
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-14 12:32:56 +0530

    hmoobhub issue fixed

--- app/Console/Commands/RunSingleSeederCommand.php
+++ app/Console/Commands/RunSingleSeederCommand.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Console\Commands;
-
+use DB;
+use Config;
+use Exception;
+use App\Models\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Config;
-use App\Models\Client;
-use Exception;
 
 class RunSingleSeederCommand extends Command
 {
@@ -46,23 +46,31 @@ class RunSingleSeederCommand extends Command
         $clients = Client::all();
         foreach ($clients as $key => $client) {
             $database_name = 'royo_' . $client->database_name;
-            $default = [
-                'driver' => env('DB_CONNECTION', 'mysql'),
-                'host' => env('DB_HOST'),
-                'port' => env('DB_PORT'),
-                'database' => $database_name,
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
-                'prefix_indexes' => true,
-                'strict' => false,
-                'engine' => null
-            ];
-            Config::set("database.connections.$database_name", $default);
-            Artisan::call('db:seed', ['--class'=>$seeder_name,'--database' => $database_name]);
-            \DB::disconnect($database_name);
+            $this->info("select database start: {$database_name}!");
+            $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME =  ?";
+            $db = DB::select($query, [$database_name]);
+            if ($db) {
+                $default = [
+                    'driver' => env('DB_CONNECTION', 'mysql'),
+                    'host' => env('DB_HOST'),
+                    'port' => env('DB_PORT'),
+                    'database' => $database_name,
+                    'username' => env('DB_USERNAME'),
+                    'password' => env('DB_PASSWORD'),
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                    'prefix' => '',
+                    'prefix_indexes' => true,
+                    'strict' => false,
+                    'engine' => null
+                ];
+                Config::set("database.connections.$database_name", $default);
+                Artisan::call('db:seed', ['--class'=>$seeder_name,'--database' => $database_name]);
+                DB::disconnect($database_name);
+            }else{
+                DB::disconnect($database_name);
+                $this->info("migrate database end: {$database_name}!");
+            }
         }
 
         return 0;
