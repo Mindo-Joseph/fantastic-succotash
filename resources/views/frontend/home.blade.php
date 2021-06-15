45852641064d81863f4e7b6de77915ffeacfb0e0
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-15 18:41:41 +0530

    conflict resolved after merge

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -120,12 +120,11 @@
                             <div class="front">
                                 <a href="{{route('vendorDetail', $vendor->id)}}"><img
                                         class="img-fluid blur-up lazyload bg-img" alt=""
-                                        src="{{$vendor->logo['proxy_url'] . '300/300' . $vendor->logo['image_path']}}"></a>
+                                        src="{{$vendor->logo['proxy_url'] . '500/500' . $vendor->logo['image_path']}}"></a>
                             </div>
                             <div class="back">
-                                <a href="{{route('vendorDetail', $vendor->id)}}"><img
-                                        class="img-fluid blur-up lazyload bg-img" alt=""
-                                        src="{{$vendor->logo['proxy_url'] . '300/300' . $vendor->logo['image_path']}}"></a>
+                                <a href="{{route('vendorDetail', $vendor->id)}}"><img class="img-fluid blur-up lazyload bg-img" alt=""
+                                        src="{{$vendor->logo['proxy_url'] . '500/500' . $vendor->logo['image_path']}}"></a>
                             </div>
                         </div>
                         <div class="product-detail">
