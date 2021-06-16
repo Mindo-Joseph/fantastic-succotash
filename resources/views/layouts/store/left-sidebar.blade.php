db4c245ba3abe7ed45a1db7028fbe348fc590dc0
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-06-16 16:09:28 +0530

    delivery address selection changes on popup

--- resources/views/layouts/store/left-sidebar.blade.php
+++ resources/views/layouts/store/left-sidebar.blade.php
@@ -246,9 +246,87 @@ $currencyList = \App\Models\ClientCurrency::with('currency')->orderBy('is_primar
     @endif
 
     <div class="search_warpper">
-
+        <div class="container">
+            @if( (session('deliveryAddress')) && (Route::currentRouteName() != 'userHome') )
+                <div class="row no-gutters" id="location_search_wrapper">
+                    <div class="col-lg-12 col-md-12 col">
+                        <div class="d-flex align-items-center justify-content-start px-3 dropdown-toggle" href="#edit-address" data-toggle="modal">
+                            <div class="map-icon mr-1"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                            <div class="homepage-address text-left">
+                                <h2><span data-placement="top" data-toggle="tooltip" title="{{session('deliveryAddress')}}">{{session('deliveryAddress')}}</span></h2>
+                            </div>
+                            <div class="down-icon">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </dv>
     </div>
 </header>
+
+<div class="modal fade edit_address" id="edit-address" tabindex="-1" aria-labelledby="edit-addressLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-body p-0">
+        <div id="address-map-container">
+            <div id="address-map"></div>
+        </div>
+        <div class="delivery_address p-2 position-relative">
+            <div class="modal-title">Set your delivery location</div>
+            <button type="button" class="close edit-close position-absolute" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <div class="form-group">
+                <label class="delivery-head">DELIVERY AREA</label>
+                <?php /* ?><div class="select_address border d-flex align-items-center justify-content-between ">
+                    <div class="location-area">
+                        <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                        <span>Sector 28 C, Chandigarh, India</span>
+                    </div>   
+                    <label class="m-0 text-uppercase">Change</label>
+                </div><?php */ ?>
+                <div class="address-input-field d-flex align-items-center justify-content-between">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    <input class="form-control border-0 map-input" type="text" name="address-input" id="address-input" value="{{session('deliveryAddress')}}">
+                    <input type="hidden" name="address_latitude" id="address-latitude" value="{{session('latitude')}}" />
+                    <input type="hidden" name="address_longitude" id="address-longitude" value="{{session('longitude')}}" />
+                </div>
+                <?php /* ?><div class="edit-area">
+                    <input class="form-control" type="text" placeholder="Complete Address *" name="complete_address" id="complete_address">
+                    <input class="form-control" type="text" placeholder="Floor (Optional)" name="floor" id="floor">
+                    <input class="form-control" type="text" placeholder="How to reach (Optional)" name="address_hint" id="address_hint">
+                </div>
+                <div class="mt-2 mb-2">
+                    <div class="address_type">
+                        <label class="radio d-inline-block m-0">Home
+                            <input type="radio" name="address_type" checked="checked" value="home">
+                            <span class="checkround"></span>
+                        </label>
+                        <label class="radio d-inline-block m-0">Office
+                            <input type="radio" name="address_type" value="office">
+                            <span class="checkround"></span>
+                        </label>
+                        <label class="radio other_address d-inline-block m-0">Other
+                            <input type="radio" name="address_type" value="other">
+                            <span class="checkround"></span>
+                        </label>   
+                    </div>
+                    <div class="other-address-input d-none">
+                        <label class="d-inline-block m-0">
+                            <input type="text" name="other_address">
+                        </label>
+                    </div>                      
+                </div><?php */ ?>
+            </div>
+            <div class="text-center">
+                <button type="button" class="btn btn-solid ml-auto confirm_address_btn">Confirm And Proceed</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script type="text/javascript">
     var show_cart_url = "{{ route('showCart') }}";
     var user_checkout_url= "{{ route('user.checkout') }}";
