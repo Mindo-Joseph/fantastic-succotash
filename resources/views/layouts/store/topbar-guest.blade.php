8469c1a139b72a50afc861e0da7bbb4957f8a92e
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-16 19:40:16 +0530

    changes in topbar

--- resources/views/layouts/store/topbar-guest.blade.php
+++ resources/views/layouts/store/topbar-guest.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-4">
                 <div class="header-contact">
                     <ul>
-                        <li>Welcome to Our store {{session('client_config')->company_name}}</li>
+                        <li>Welcome to Our store {{session('client_config') ? session('client_config')->company_name : ''}}</li>
                         <li><i class="fa fa-phone" aria-hidden="true"></i>Call Us: {{session('client_config')->phone_number}}</li>
                     </ul>
                 </div>
