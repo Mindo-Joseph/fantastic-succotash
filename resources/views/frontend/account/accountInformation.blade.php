cdbd373b1fec0bd4b8c0505d15cb5abc19a613aa
chander_codebrew <chandar@codebrewinnovations.com>
2021-06-17 18:24:28 +0530

    wallet screen done

--- resources/views/frontend/account/accountInformation.blade.php
+++ resources/views/frontend/account/accountInformation.blade.php
@@ -99,61 +99,65 @@
                             <li><a href="{{route('user.addressBook')}}">Address Book</a></li>
                             <li><a href="{{route('user.orders')}}">My Orders</a></li>
                             <li><a href="{{route('user.wishlists')}}">My Wishlist</a></li>
-                            <li class="active"><a href="{{route('user.account')}}">My Account</a></li>
+                            <li class="active"><a href="{{route('user.account')}}">My Wallet</a></li>
                             <li><a href="{{route('user.changePassword')}}">Change Password</a></li>
                             <li class="last"><a href="{{route('user.logout')}}">Log Out</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-7">
+            <div class="col-lg-9">
                 <div class="dashboard-right">
                     <div class="dashboard">
                         <div class="page-title">
                             <h3 class="mt-0">My Wallet</h3>
                         </div>
 
-                        <div class="box-account box-info 12">
+                        <div class="box-account box-info">
 
-                            <div class="card-box">
-                                <form>
-                                    <div class="row mb-3">
-                                        <div class="col-12 d-flex align-items-center">
-                                            <div class='file file--upload'>
-                                                <label for='input-file'>
-                                                    <span class="update_pic">
-                                                        <img src="{{asset('assets/images/products/product-1.png')}}" alt=""
-                                                            id="output">
-                                                    </span>
-                                                    <span class="plus_icon"><i class="fa fa-camera"
-                                                            aria-hidden="true"></i></span>
-                                                </label>
-                                                <input id='input-file' type='file' name="profile_image" accept="image/*"
-                                                    onchange="loadFile(event)" />
-                                            </div>
-                                            <div class="name_location">
-                                                <h5 class="mt-0 mb-1">CM Azad</h5>
-                                                <p class="m-0"><i class="fa fa-map-marker mr-1" aria-hidden="true"></i>
-                                                    Chandigarh</p>
-                                            </div>
-                                        </div>
+                            <div class="card-box mb-0">
+                                <div class="row align-items-center">
+                                    <div class="col-sm-9 text-sm-left text-center mb-md-0 mb-4">
+                                        <h5 class="text-17 mb-2">Available Balance</h5>
+                                        <div class="text-36">$0</div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="">Your Name</label>
-                                        <input type="text" class="form-control" id="" aria-describedby="">
+                                    <div class="col-sm-3 text-sm-right text-center">
+                                        <a class="btn btn-solid" href="#" data-toggle="modal" data-target="#add-money">Payout</a>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="">Email address</label>
-                                        <input type="email" class="form-control" id="" aria-describedby="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">A little bit about yourself</label>
-                                        <textarea class="form-control" name="" id="" cols="20" rows="10"></textarea>
-                                    </div>
-                                    <button type="submit" class="btn btn-solid mt-2 w-100">Submit</button>
-                                </form>
+                                </div>
                             </div>
 
+                            <h6>Transaction History</h6>
+
+                            <div class="card-box">
+                              <div class="table-responsive">
+                                  <table class="table wallet-tarnsaction w-100">
+                                      <tbody>
+                                          <tr>
+                                              <td>Aug 12, 2021   09:20 am</td>
+                                              <td>Recieved from Shane colin</td>
+                                              <td class="text-right"><b>+$50.00</b></td>
+                                          </tr>
+                                          <tr>
+                                              <td>Aug 12, 2021   09:20 am</td>
+                                              <td>Recieved from Shane colin</td>
+                                              <td class="text-right"><b>+$50.00</b></td>
+                                          </tr>
+                                          <tr>
+                                              <td>Aug 12, 2021   09:20 am</td>
+                                              <td>Recieved from Shane colin</td>
+                                              <td class="text-right"><b>+$50.00</b></td>
+                                          </tr>
+                                          <tr>
+                                              <td>Aug 12, 2021   09:20 am</td>
+                                              <td>Recieved from Shane colin</td>
+                                              <td class="text-right"><b>+$50.00</b></td>
+                                          </tr>
+                                          
+                                      </tbody>
+                                  </table>
+                              </div>
+                          </div>
                             <!-- <div class="box-head">
                                 <h2>Account Information</h2>
                             </div> -->
@@ -232,6 +236,41 @@
         </div>
     </div>
 </section>
+
+<!-- Modal -->
+<div class="modal fade wallet_money" id="add-money" tabindex="-1" aria-labelledby="add-moneyLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header border-bottom">
+        <h5 class="modal-title" id="add-moneyLabel">Pay-Out</h5>
+        <button type="button" class="close top_right" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+            <div class="form-group">
+                <label for="">Account Number</label>
+                <input class="form-control" type="text" placeholder="Account Number">
+            </div>
+            <div class="form-group">
+                <label for="">Account Name</label>
+                <input class="form-control" type="text" placeholder="Account Name">
+            </div>
+            <div class="form-group">
+                <label for="">Bank Name</label>
+                <input class="form-control" type="text" placeholder="Bank Name">
+            </div>
+            <div class="form-group">
+                <label for="">IFSC Code</label>
+                <input class="form-control" type="text" placeholder="IFSC Code">
+            </div>
+            <button type="button" class="btn btn-solid w-100 mt-2" data-dismiss="modal">Close</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
 
