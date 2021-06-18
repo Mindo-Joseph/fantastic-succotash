a7be4c542b4136539c586941321fc07ad7038bd8
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-06-18 19:40:37 +0530

    icon file included

--- resources/views/layouts/store/head-content.blade.php
+++ resources/views/layouts/store/head-content.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/font-awesome.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/themify-icons.css')}}">
 <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/color1.css')}}" media="screen" id="color">
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/style.css')}}">
