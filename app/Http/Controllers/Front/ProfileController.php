88b4a15b36377ea3a6791bdea27abf2b398115d8
Puneet <puneet.g@codebrewinnovations.com>
2021-06-18 14:33:59 +0530

    change in migrations for refferal table

--- app/Http/Controllers/Front/ProfileController.php
+++ app/Http/Controllers/Front/ProfileController.php
@@ -62,30 +62,7 @@ class ProfileController extends FrontController
         }
         return response()->json(array('success' => true, 'message' => 'Send Successfully'));
     }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function addresBook(Request $request, $domain = ''){
-        $langId = Session::get('customerLanguage');
-        $useraddress = UserAddress::where('user_id', Auth::user()->id)->with('country')->get();
-        $navCategories = $this->categoryNav($langId);
-        return view('frontend/account/addressbook')->with(['useraddress' => $useraddress, 'navCategories' => $navCategories]);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function orders(Request $request, $domain = ''){
-        $langId = Session::get('customerLanguage');
-        $navCategories = $this->categoryNav($langId);
-       return view('frontend/account/orders')->with(['navCategories' => $navCategories]);
-    }
-
+    
     /**
      * Display a listing of the resource.
      *
@@ -139,17 +116,5 @@ class ProfileController extends FrontController
         }
         return redirect()->route('user.profile');
     }
-
-    /** User account information        */
-    public function walletInformation(Request $request, $domain = ''){
-        $langId = Session::get('customerLanguage');
-        $user = User::with('country')->find(Auth::user()->id);
-        $navCategories = $this->categoryNav($langId);
-        $auth_user = Auth::user();
-        $user_transactions = Transaction::where('payable_id', $auth_user->id)->get();
-        return view('frontend/account/wallet')->with(['user' => $user, 'navCategories' => $navCategories, 'user_transactions' => $user_transactions]);
-    }
-
-    
 
 }
