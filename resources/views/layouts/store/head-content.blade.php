fc05488fea19496a0036bb4c63e428b96a746145
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-18 17:12:03 +0530

    remove js and css extra file

--- resources/views/layouts/store/head-content.blade.php
+++ resources/views/layouts/store/head-content.blade.php
@@ -2,7 +2,6 @@
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/font-awesome.min.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/slick.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/slick-theme.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/themify-icons.css')}}">
 <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/color1.css')}}" media="screen" id="color">
 <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/style.css')}}">
