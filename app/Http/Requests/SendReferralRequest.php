e50e15aff664ae3038de1211379e321e0db26d87
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-18 14:52:37 +0530

    changes in refferal request

--- app/Http/Requests/SendReferralRequest.php
+++ app/Http/Requests/SendReferralRequest.php
@@ -11,7 +11,7 @@ class SendReferralRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize() 
     {
         return Auth::check();
     }
