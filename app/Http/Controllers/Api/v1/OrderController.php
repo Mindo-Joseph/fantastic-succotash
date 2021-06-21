191bf9324517fef545671e64583896589010d623
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-21 11:01:12 +0530

    changes in order controller

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -39,14 +39,27 @@ class OrderController extends Controller {
     		$order_item_count = 0;
     		$order_id = $request->order_id;
             $vendor_id = $request->vendor_id;
-            'vendorProducts.product.translation' => function($q) use($langId){
-                        $q->select('product_id', 'title', 'body_html', 'meta_title', 'meta_keyword', 'meta_description');
-                        $q->where('language_id', $langId);
-                    },
+            $user = Auth::user();
+            $language_id = $user->language;
             if($vendor_id){
-	       	   $order = Order::with(['vendors' => function($q) use($vendor_id){$q->where('vendor_id', $vendor_id);},'vendors.products' => function($q) use($vendor_id){$q->where('vendor_id', $vendor_id);},'vendors.products.pvariant.vset.optionData.trans','vendors.products.addon','vendors.coupon','address'])->where('id', $order_id)->first();
+	       	   $order = Order::with([
+                'vendors' => function($q) use($vendor_id){$q->where('vendor_id', $vendor_id);},
+                'vendors.products' => function($q) use($vendor_id){$q->where('vendor_id', $vendor_id);},
+                'vendors.products.translation' => function($q) use($language_id){
+                    $q->select('product_id', 'title', 'body_html', 'meta_title', 'meta_keyword', 'meta_description');
+                    $q->where('language_id', $language_id);
+                },
+                'vendors.products.pvariant.vset.optionData.trans','vendors.products.addon','vendors.coupon','address']
+                )->where('id', $order_id)->first();
             }else{
-                $order = Order::with(['vendors.vendor','vendors.products' => function($q) use($order_id){$q->where('order_id', $order_id);},'vendors.products' => function($q) use($order_id){$q->where('order_id', $order_id);},'vendors.products.pvariant.vset.optionData.trans','vendors.products.addon','vendors.coupon','address'])->where('user_id', $user->id)->where('id', $order_id)->first();
+                $order = Order::with(['vendors.vendor',
+                    'vendors.products' => function($q) use($order_id){$q->where('order_id', $order_id);},
+                    'vendors.products' => function($q) use($order_id){$q->where('order_id', $order_id);},
+                    'vendors.products.translation' => function($q) use($language_id){$q->select('product_id', 'title', 'body_html', 'meta_title', 'meta_keyword', 'meta_description');
+                        $q->where('language_id', $language_id);
+                    },
+                    'vendors.products.pvariant.vset.optionData.trans','vendors.products.addon','vendors.coupon','address']
+                )->where('user_id', $user->id)->where('id', $order_id)->first();
             }
             if($order->vendors){
                 $order->user_name = $order->user->name;
