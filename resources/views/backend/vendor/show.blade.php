c6e3173d4d24139fdc14bf999c3ed00d98d3cfcb
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-06-21 18:16:28 +0530

    vendor service area and name validation added and nav categories query change

--- resources/views/backend/vendor/show.blade.php
+++ resources/views/backend/vendor/show.blade.php
@@ -44,6 +44,15 @@
                         <span>{!! \Session::get('error_delete') !!}</span>
                     </div>
                     @endif
+                    @if ( ($errors) && (count($errors) > 0) )
+                        <div class="alert alert-danger">
+                            <ul class="m-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
