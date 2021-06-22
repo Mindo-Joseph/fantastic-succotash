56504916a7fbf576e7148c2af987cb6ea27b7aa7
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-22 11:30:03 +0530

    Order minimum amount per vendor validation

--- app/Http/Controllers/Front/PaymentController.php
+++ app/Http/Controllers/Front/PaymentController.php
@@ -7,7 +7,7 @@ use Omnipay\Omnipay;
 use App\Models\Payment;
 use App\Models\PaymentOption;
 use Illuminate\Http\Request;
-use App\Models\{Order, User};
+use App\Models\{Order, User, Cart, ClientCurrency, CartProduct};
 use App\Http\Traits\ApiResponser;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Front\FrontController;
@@ -22,81 +22,58 @@ class PaymentController extends FrontController{
      * @return \Illuminate\Http\Response
      */
     public function index(){
+        $currency_symbol = Session::get('currencySymbol');
+        $user = Auth::user();
+        $vendor_min_amount_errors = [];
+        $cart = Cart::where('user_id', $user->id)->first();
+        if($cart){
+            $clientCurrency = ClientCurrency::where('currency_id', $user->currency)->first();
+            $cart_products = CartProduct::with('vendor','product.pimage', 'product.variants', 'product.taxCategory.taxRate','coupon', 'product.addon')->where('cart_id', $cart->id)->where('status', [0,1])->where('cart_id', $cart->id)->orderBy('created_at', 'asc')->get();
+            $dollar_compare = $clientCurrency ? $clientCurrency->doller_compare : 1;
+            foreach ($cart_products->groupBy('vendor_id') as $vendor_id => $vendor_cart_products) {
+                $vendor_detail = [];
+                $vendor_payable_amount = 0;
+                $vendor_discount_amount = 0;
+                foreach ($vendor_cart_products as $vendor_cart_product) {
+                    $vendor_detail = $vendor_cart_product->vendor;
+                    $variant = $vendor_cart_product->product->variants->where('id', $vendor_cart_product->variant_id)->first();
+                    $quantity_price = 0;
+                    $divider = $vendor_cart_product->doller_compare ? $vendor_cart_product->doller_compare : 1;
+                    $price_in_currency = $variant->price / $divider;
+                    $price_in_dollar_compare = $price_in_currency * $dollar_compare;
+                    $quantity_price = $price_in_dollar_compare * $vendor_cart_product->quantity;
+                    $vendor_payable_amount = $vendor_payable_amount + $quantity_price;
+                    $product_taxable_amount = 0;
+                    $product_payable_amount = 0;
+                    if(!empty($vendor_cart_product->addon)){
+                        foreach ($vendor_cart_product->addon as $ck => $addon) {
+                            $opt_quantity_price = 0;
+                            $opt_price_in_currency = $addon->option->price;
+                            $opt_price_in_doller_compare = $opt_price_in_currency * $clientCurrency->doller_compare;
+                            $opt_quantity_price = $opt_price_in_doller_compare * $order_product->quantity;
+                            $vendor_payable_amount = $vendor_payable_amount + $opt_quantity_price;
+                        }
+                    }
+                }
+                if($vendor_detail){
+                    if($vendor_detail->order_min_amount > 0){
+                        if($vendor_payable_amount < $vendor_detail->order_min_amount){
+                            $vendor_min_amount_errors[]= array(
+                                'vendor_id' => $vendor_detail->id,
+                                'message' => "Minimum order should be more than  $currency_symbol $vendor_detail->order_min_amount",
+                            );
+                        }
+                    }
+                }
+            }
+            if(count($vendor_min_amount_errors) > 0){
+                return $this->errorResponse($vendor_min_amount_errors, 402);
+            }
+        }
         $payment_options = PaymentOption::select('id', 'code', 'title')->where('status', 1)->get();
         foreach ($payment_options as $payment_option) {
            $payment_option->slug = strtolower(str_replace(' ', '_', $payment_option->title));
         }
         return $this->successResponse($payment_options);
-    }
-    public function showForm()
-    {
-        return view('backend/stripe/form');
-    }
-
-    public function showFormApp($domain='',$token = '')
-    {
-        $uid = 0;
-        if(Auth::user()){
-            $uid = Auth::user()->id;
-        }elseif(!empty($token)){
-            $userCart = Cart::where('uniqueIdenteri', $token)->first();
-            if(!$userCart){
-                // sent to error page
-            }
-            $uid = $userCart->user_id;
-        }else{
-            // sent to error page
-        }
-
-        $user = User::select()->where('id', $uid);
-
-        $userId = Auth::user()->id;
-        // dd($token);
-        $langId = Session::get('customerLanguage');
-        $navCategories = $this->categoryNav($langId);
-        return view('frontend.payment')->with(['navCategories' => $navCategories]);
-    }
-
-    public function makePayment(Request $request)
-    {
-        // dd($request->all());
-
-        $token = $request->stripeToken;
-
-        // Setup payment gateway
-        $gateway = Omnipay::create('Stripe');
-        $gateway->setApiKey('sk_test_51IhpwhSFHEA938FwRPiQSAH5xF6DcjO5GCASiud9cGMJ0v8UJyRfCb7IQAMbXbuPMe7JphA1izxZOsIclvmOgqUV00Zpk85xfl');
-
-        // Example form data
-        $formData = [
-            'number' => '4000056655665556',
-            'description' => '4242424242424242',
-            'expiryMonth' => '6',
-            'expiryYear' => '2026',
-            'cvv' => '123'
-        ];
-
-        // try {
-
-        // Send purchase request
-        $response = $gateway->purchase(
-            [
-                'amount' => '10.00',
-                'currency' => 'INR',
-                'card' => $formData,
-                'token' => $token,
-            ]
-        )->send();
-
-            if ($response->isSuccessful()) {
-                // mark order as complete
-                dd("successfull");
-            } elseif ($response->isRedirect()) {
-                $response->redirect();
-            } else {
-                // display error to customer
-                exit($response->getMessage());
-            }
-       
     }
 }
