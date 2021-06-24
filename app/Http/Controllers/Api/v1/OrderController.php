45f3cceae2de4809b0f3ad5327461a4934f7be89
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-24 15:57:19 +0530

    changes in order controller

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -40,7 +40,7 @@ class OrderController extends Controller {
             case 'schedule':
                 $order_status_options = [10];
                 $orders->whereHas('status', function ($query) use($order_status_options) {
-                    $query->whereNotIn('order_status_option_id', $order_status_options);
+                    $query->whereIn('order_status_option_id', $order_status_options);
                 });
             break;
         }
