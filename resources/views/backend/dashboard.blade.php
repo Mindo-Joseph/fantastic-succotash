2f2b2dde8313a41c730347e5867832d4203467d4
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-24 19:35:20 +0530

    changes in footer [page

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1029,7 +1029,7 @@
 <script src="{{asset('assets/libs/flatpickr/flatpickr.min.js')}}"></script>
 
 <!-- Third Party js-->
-<script src="{{asset('assets/js/vendor.min.js')}}"></script>
+<!-- <script src="{{asset('assets/js/vendor.min.js')}}"></script> -->
 
 <!-- Plugins js-->
 <script src="{{asset('assets/libs/admin-resources/admin-resources.min.js')}}"></script>
