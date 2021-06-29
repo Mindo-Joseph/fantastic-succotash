d6e529d076af437f5c8663c87fd2529f587cdcd4
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-06-29 11:43:39 +0530

    changes in accounting order detail

--- resources/views/backend/accounting/order.blade.php
+++ resources/views/backend/accounting/order.blade.php
@@ -4,7 +4,6 @@
 <link href="{{asset('assets/libs/dropzone/dropzone.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('assets/libs/dropify/dropify.min.css')}}" rel="stylesheet" type="text/css" />
 @endsection
-
 @section('content')
 <div class="content">
     <div class="container-fluid">
@@ -25,32 +24,29 @@
                             <div class="col-sm-6 col-md-3 mb-3 mb-md-0">
                                 <div class="text-center">
                                     <i class="mdi mdi-cart-plus text-primary mdi-24px"></i>
-                                    <h3><span data-plugin="counterup">8954</span></h3>
-                                    <p class="text-muted font-15 mb-0">Total earnings by vendors</p>
+                                    <h3><span data-plugin="counterup" id="total_earnings_by_vendors">0</span></h3>
+                                    <p class="text-muted font-15 mb-0">Total Earnings By Vendors</p>
                                 </div>
                             </div>
-
                             <div class="col-sm-6 col-md-3 mb-3 mb-md-0">
                                 <div class="text-center">
                                     <i class="mdi mdi-currency-usd text-success mdi-24px"></i>
-                                    <h3>$ <span data-plugin="counterup">7841</span></h3>
-                                    <p class="text-muted font-15 mb-0">Total orders</p>
+                                    <h3>$ <span data-plugin="counterup" id="total_order_count">0</span></h3>
+                                    <p class="text-muted font-15 mb-0">Total Orders</p>
                                 </div>
                             </div>
-
                             <div class="col-sm-6 col-md-3 mb-3 mb-md-0">
                                 <div class="text-center">
                                     <i class="mdi mdi-account-group text-danger mdi-24px"></i>
-                                    <h3><span data-plugin="counterup">6521</span></h3>
-                                    <p class="text-muted font-15 mb-0">Total cash to be collected</p>
+                                    <h3><span data-plugin="counterup" id="total_cash_to_collected">0</span></h3>
+                                    <p class="text-muted font-15 mb-0">Total Cash To Be Collected</p>
                                 </div>
                             </div>
-
                             <div class="col-sm-6 col-md-3 mb-3 mb-md-0">
                                 <div class="text-center">
                                     <i class="mdi mdi-eye-outline text-blue mdi-24px"></i>
-                                    <h3><span data-plugin="counterup">{{round($delivery_fee_sum)}}</span> k</h3>
-                                    <p class="text-muted font-15 mb-0">total delivery fees</p>
+                                    <h3><span data-plugin="counterup" id="total_delivery_fees">0</span> k</h3>
+                                    <p class="text-muted font-15 mb-0">Total Delivery Fees</p>
                                 </div>
                             </div>
                         </div>
@@ -60,4 +56,117 @@
         </div>    
     </div> 
 </div>
+<script type="text/template" id="accounting_vendor_template">
+    <% _.each(vendor_orders, function(vendor_order, key){%>
+        <tr data-row-id="1">
+            <td><%= vendor_order.order_detail.order_number%></td>
+            <td><%= vendor_order.created_at %></td>
+            <td><%= vendor_order.user.name %></td>
+            <td><%= vendor_order.vendor.name %></td>
+            <td><%= vendor_order.payable_amount %></td>
+            <td><%= vendor_order.discount_amount %></td>
+            <td></td>
+            <td></td>
+            <td></td> 
+            <td><%= vendor_order.order_detail.payment_option.title %></td> 
+        </tr>
+    <% }); %>
+</script>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body position-relative">
+                    <div class="top-input position-absolute">
+                        <div class="row">
+                            <div class="col-md-6">
+                                 <input type="text" class="form-control" data-provide="datepicker" data-date-format="MM yyyy" data-date-min-view-mode="1" id="month_picker_filter" style="display:none;">
+                            </div>
+                        </div>  
+                   </div>
+                    <div class="table-responsive">
+                        <table class="table table-centered table-nowrap table-striped" id="accounting_vendor_datatable">
+                            <thead>
+                                <tr>
+                                    <th>Order Id</th>
+                                    <th>Date & Time</th>
+                                    <th>Customer Name</th>
+                                    <th>Vendor</th>
+                                    <th>Subtotal amount</th>
+                                    <th>Promo Code Discount</th>
+                                    <th>Admin Commission [Fixed]</th>
+                                    <th>Admin Commission [%age]</th>
+                                    <th>Final Amount</th>
+                                    <th>Payment Method</th>
+                                </tr>
+                            </thead>
+                            <tbody id="accounting_vendor_tbody_list">
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var table;
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('input[name="_token"]').val()
+            }
+        });
+        getOrderList();
+        function getOrderList() {
+            $.ajax({
+                data: {},
+                type: "POST",
+                dataType: "json",
+                url: "{{route('account.order.filter')}}",
+                success: function(response) {
+                    if(response.status == 'Success'){
+                        // $('#month_picker_filter').show();
+                        $('#accounting_vendor_tbody_list').html('');
+                        let accounting_vendor_template = _.template($('#accounting_vendor_template').html());
+                        $("#accounting_vendor_tbody_list").append(accounting_vendor_template({vendor_orders: response.data.vendor_orders}));
+
+                        $('#total_order_count').html(response.data.vendor_orders.length);
+                        $('#total_delivery_fees').html(response.data.total_delivery_fees);
+                        $('#total_cash_to_collected').html(response.data.total_cash_to_collected);
+                        $('#total_earnings_by_vendors').html(response.data.total_earnings_by_vendors);
+
+                        $('#total_order_count').counterUp({
+                          delay: 10,
+                          time: 2000
+                        });
+                        $('#total_delivery_fees').counterUp({
+                          delay: 10,
+                          time: 2000
+                        });
+                        $('#total_cash_to_collected').counterUp({
+                          delay: 10,
+                          time: 2000
+                        });
+                        $('#total_earnings_by_vendors').counterUp({
+                          delay: 10,
+                          time: 2000
+                        });
+                        // if($.fn.DataTable.isDataTable('#accounting_vendor_datatable')){
+                        //     table.destroy();
+                        //     $('#accounting_vendor_datatable tbody').empty();
+                        // }
+                        // table = $("#accounting_vendor_datatable").DataTable({
+                        //     "dom": '<"toolbar">frtip',
+                        //     drawCallback: function () {
+                        //         $(".dataTables_paginate > .pagination").addClass("pagination-rounded");
+                        //     },
+                        // }).fnClearTable();
+                    }
+                }
+            });
+        }
+    });
+</script>
 @endsection
