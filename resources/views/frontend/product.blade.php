98ffb9825ca8ddbf0fa0575de968199f44604a79
chander_codebrew <chandar@codebrewinnovations.com>
2021-07-02 17:23:58 +0530

    rating page html

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -330,7 +330,50 @@
                                         aria-labelledby="review-top-tab">
                                         {{-- <a class="btn btn-solid add_edit_review" href="javascript:void(0)" data-id="8">Rate</a>
                                                --}}
-                                       
+                                        <div v-for="item in list" class="review d-flex justify-content-between mb-3 mb-lg-4">    
+                                            <div class="review-photo">
+                                                <img src="https://imgproxy.royoorders.com/insecure/fill/300/300/sm/0/plain/https://s3.us-west-2.amazonaws.com/royoorders2.0-assets/prods/itMxicEDPKgiIlq0uIrIpDWk7UnfSMpXiuPDlorJ.png">
+                                            </div>
+                                            <div class="review-box">      
+                                                <div class="review-author">
+                                                    <p><strong>CM Azad</strong> - <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    - Chandigrah</p>
+                                                </div>
+                                                <div class="review-comment">
+                                                    <p>Lorem ipsum dolor sit amet, cu pertinax nominati sea, id iriure utamur interpretaris sea, vix ne latine aliquam complectitur. Enim quas his no, mea ferri audire rationibus ei.</p>
+                                                </div>
+
+                                                <div class="review-date">
+                                                    <time>July, 27th, 2016</time>
+                                                </div>      
+                                            </div>
+                                        </div>    
+                                        <div v-for="item in list" class="review d-flex justify-content-between mb-3 mb-lg-4">    
+                                            <div class="review-photo">
+                                                <img src="https://imgproxy.royoorders.com/insecure/fill/300/300/sm/0/plain/https://s3.us-west-2.amazonaws.com/royoorders2.0-assets/prods/itMxicEDPKgiIlq0uIrIpDWk7UnfSMpXiuPDlorJ.png">
+                                            </div>
+                                            <div class="review-box">      
+                                                <div class="review-author">
+                                                    <p><strong>CM Azad</strong> - <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    <i class="fa fa-star" aria-hidden="true"></i>
+                                                    - Chandigrah</p>
+                                                </div>
+                                                <div class="review-comment">
+                                                    <p>Lorem ipsum dolor sit amet, cu pertinax nominati sea, id iriure utamur interpretaris sea, vix ne latine aliquam complectitur. Enim quas his no, mea ferri audire rationibus ei.</p>
+                                                </div>
+
+                                                <div class="review-date">
+                                                    <time>July, 27th, 2016</time>
+                                                </div>      
+                                            </div>
+                                        </div>    
                                     </div>
                                 </div>
                             </div>
