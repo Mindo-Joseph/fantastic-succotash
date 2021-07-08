38ffcf6fb9eb77f3de2cf5bf248a1d9a8a55d4f0
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-07-08 15:02:48 +0530

    changes in page controller

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+use DB;
+use App\Models\Page;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pages')->truncate();
+        $page_array = ['Privacy Policy', 'Terms & Conditions'];
+        foreach ($page_array as $page) {
+            Page::create(['title' => $page, 'description' => 'We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use. By browsing the public areas of the Website, you acknowledge that you have read, understood, and agree to be legally bound by these Terms of Use and our Privacy Policy, which is hereby incorporated by reference (collectively, this “Agreement”). If you do not agree to any of these terms, then please do not use the Website, the App, and/or the Platform. We may change the terms and conditions of these Terms of Use from time to time with or without notice to you.']);
+        }
+    }
+}
