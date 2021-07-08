85e0887279f5f5a58254d56120ed5ee8c561499a
chander_codebrew <chandar@codebrewinnovations.com>
2021-07-08 11:33:41 +0530

    cms page done

--- resources/views/backend/cms/page/index.blade.php
+++ resources/views/backend/cms/page/index.blade.php
@@ -12,30 +12,61 @@
                 <h4 class="page-title">Page</h4>
             </div>
         </div>
-        <div class="col-4 text-right" style="margin: auto;">
-            <button type="button" class="btn btn-info waves-effect waves-light text-sm-right saveProduct"> Submit</button>
-        </div>
+        
     </div>
 
     <div class="row">
         <div class="col-lg-3 mb-2">
             <div class="card">
                 <div class="card-body">
-                    <div class="text-right">
-                        <button class="btn btn-info">Add Page</button>
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h4>Pages List</h4>
+                        <button class="btn btn-info">Add New Page</button>
                     </div> 
-                   <ul>
-                        <li>
-                            <a href=""></a>
-
-                        </li>
-                   </ul>
+                   <div class="table-responsive pages-list-data">
+                        <table class="table table-striped w-100">
+                            <thead>
+                                <tr>
+                                    <th class="border-bottom-0">Page Name</th>
+                                    <th class="text-right border-bottom-0">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <a class="text-body" href="#">Terms & Conditions</a>
+                                    </td>
+                                    <td align="right">
+                                        <a class="text-body" href="#"><i class="mdi mdi-square-edit-outline"></i></a>
+                                        <a class="text-body" href="#"><i class="mdi mdi-delete"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a class="text-body" href="#">Privacy Policy</a>
+                                    </td>
+                                    <td align="right">
+                                        <a class="text-body" href="#"><i class="mdi mdi-square-edit-outline"></i></a>
+                                        <a class="text-body" href="#"><i class="mdi mdi-delete"></i></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                   </div>
                 </div>            
             </div>
         </div>
         <div class="col-lg-9 mb-2">
             <div class="card">
                 <div class="card-body">
+                    <div class="row mb-2 align-items-center">
+                        <div class="col-8">
+                            <h4 class="m-0">Page Content</h4>
+                        </div>
+                        <div class="col-4 text-right" style="margin: auto;">
+                            <button type="button" class="btn btn-info waves-effect waves-light text-sm-right saveProduct"> Submit</button>
+                        </div>
+                    </div>
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="row">
