6385a4f44f82f8445bc3bfc20bddd4ce091ad987
chander_codebrew <chandar@codebrewinnovations.com>
2021-07-08 13:49:44 +0530

    cms page changes

--- resources/views/backend/cms/page/index.blade.php
+++ resources/views/backend/cms/page/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.vertical', ['demo' => 'creative', 'title' => 'CMS Pages'])
+@extends('layouts.vertical', ['demo' => 'creative', 'title' => 'Pages'])
 
 @section('css')
 <link href="{{asset('assets/libs/dropzone/dropzone.min.css')}}" rel="stylesheet" type="text/css" />
@@ -9,7 +9,7 @@
     <div class="row">
         <div class="col-8">
             <div class="page-title-box">
-                <h4 class="page-title">CMS Page</h4>
+                <h4 class="page-title">Pages</h4>
             </div>
         </div>
         
@@ -20,8 +20,8 @@
             <div class="card">
                 <div class="card-body p-3">
                     <div class="d-flex align-items-center justify-content-between mb-3">
-                        <h4>Page List</h4>
-                        <button class="btn btn-info" data-toggle="modal" data-target="#add_cms_page"><i class="mdi mdi-plus-circle mr-1"></i> Add New Page</button>
+                        <h4>List</h4>
+                        <button class="btn btn-info" data-toggle="modal" data-target="#add_cms_page"><i class="mdi mdi-plus-circle"></i> Add</button>
                     </div> 
                    <div class="table-responsive pages-list-data">
                         <table class="table table-striped w-100">
@@ -71,12 +71,12 @@
                         <div class="col-lg-6">
                             <div class="row">
                                 <div class="col-12 mb-2">
-                                    <label for="title" class="control-label">Page Title</label>
+                                    <label for="title" class="control-label">Title</label>
                                     <input class="form-control" id="meta_title" placeholder="Meta Title" name="meta_title" type="text">
                                 </div>
                                 <div class="col-12 mb-2">
-                                    <label for="title" class="control-label">Page Description</label>
-                                    <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="9" name="meta_description" cols="50"></textarea>
+                                    <label for="title" class="control-label">Meta Keyword</label>
+                                    <textarea class="form-control" id="meta_keyword" placeholder="Meta Keyword" rows="3" name="meta_keyword" cols="50"></textarea>
                                 </div>
                             </div>         
                         </div>
@@ -86,23 +86,42 @@
                                     <label for="title" class="control-label">Meta Title</label>
                                     <input class="form-control" id="meta_title" placeholder="Meta Title" name="meta_title" type="text">
                                 </div>
-                                <div class="col-12 mb-2">
-                                    <label for="title" class="control-label">Meta Keyword</label>
-                                    <textarea class="form-control" id="meta_keyword" placeholder="Meta Keyword" rows="3" name="meta_keyword" cols="50"></textarea>
-                                </div>
+                                
                                 <div class="col-12 mb-2">
                                     <label for="title" class="control-label">Meta Description</label>
                                     <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="3" name="meta_description" cols="50"></textarea>
                                 </div>                               
                             </div>         
                         </div>
+                        <div class="col-12 mb-2">
+                            <label for="title" class="control-label">Description</label>
+                            <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="9" name="meta_description" cols="50"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="page-content mb-3">
+                                <h6 class="text-uppercase mb-2">Lorem ipsum dolor sit amet. </h6>
+                                <p>We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use. By browsing the public areas of the Website, you acknowledge that you have read, understood, and agree to be legally bound by these Terms of Use and our Privacy Policy, which is hereby incorporated by reference (collectively, this “Agreement”). If you do not agree to any of these terms, then please do not use the Website, the App, and/or the Platform. We may change the terms and conditions of these Terms of Use from time to time with or without notice to you.</p>
+                                <p>We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use.</p>
+                            </div>
+                            <div class="page-content mb-3">
+                                <h6 class="text-uppercase mb-2">DESCRIPTION AND USE OF PLATFORM</h6>
+                                <p>We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use.</p>
+                            </div>
+                            <div class="page-content mb-3">
+                                <h6 class="text-uppercase mb-2">DESCRIPTION AND USE OF PLATFORM</h6>
+                                <p>We provide Visitors (as defined below) with access to the Website and Registered Members (as defined below) with access to the Platform subject to the following Terms of Use. By browsing the public areas of the Website, you acknowledge that you have read, understood, and agree to be legally bound by these Terms of Use and our Privacy Policy, which is hereby incorporated by reference (collectively, this “Agreement”). If you do not agree to any of these terms, then please do not use the Website, the App, and/or the Platform. We may change the terms and conditions of these Terms of Use from time to time with or without notice to you.</p>
+                                <div class="upadted_date text-uppercase">
+                                    Updated on 12/07/2020
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>            
             </div>
-        </div>
-         
+        </div>         
     </div>
-
 </div>
 
 
@@ -121,12 +140,12 @@
                 <div class="col-lg-6">
                     <div class="row">
                         <div class="col-12 mb-2">
-                            <label for="title" class="control-label">Page Title</label>
+                            <label for="title" class="control-label">Title</label>
                             <input class="form-control" id="meta_title" placeholder="Meta Title" name="meta_title" type="text">
                         </div>
                         <div class="col-12 mb-2">
-                            <label for="title" class="control-label">Page Description</label>
-                            <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="9" name="meta_description" cols="50"></textarea>
+                            <label for="title" class="control-label">Meta Keyword</label>
+                            <textarea class="form-control" id="meta_keyword" placeholder="Meta Keyword" rows="3" name="meta_keyword" cols="50"></textarea>
                         </div>
                     </div>         
                 </div>
@@ -136,16 +155,17 @@
                             <label for="title" class="control-label">Meta Title</label>
                             <input class="form-control" id="meta_title" placeholder="Meta Title" name="meta_title" type="text">
                         </div>
-                        <div class="col-12 mb-2">
-                            <label for="title" class="control-label">Meta Keyword</label>
-                            <textarea class="form-control" id="meta_keyword" placeholder="Meta Keyword" rows="3" name="meta_keyword" cols="50"></textarea>
-                        </div>
+                        
                         <div class="col-12 mb-2">
                             <label for="title" class="control-label">Meta Description</label>
                             <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="3" name="meta_description" cols="50"></textarea>
                         </div>                               
                     </div>         
                 </div>
+                <div class="col-12 mb-2">
+                            <label for="title" class="control-label">Description</label>
+                            <textarea class="form-control" id="meta_description" placeholder="Meta Description" rows="9" name="meta_description" cols="50"></textarea>
+                        </div>
                 <div class="col-12 mt-3">
                     <button type="button" class="btn btn-info w-100" data-dismiss="modal">Done</button>
                 </div>
