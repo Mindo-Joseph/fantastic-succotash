e43271acb756af7353070a58b60ba6966fc8aa0e
Puneet <puneet.g@codebrewinnovations.com>
2021-07-09 19:36:48 +0530

    catalog add popup changes

--- resources/views/backend/catalog/edit-category.blade.php
+++ resources/views/backend/catalog/edit-category.blade.php
@@ -90,7 +90,7 @@
                 @if($type->title == 'Pickup/Delivery' && $preference->takeaway_check == 0)
                     @continue
                 @endif
-               <div class="col-sm-3 col-md-4">
+               <div class="col-sm-6 col-md-4">
                   <div class="card p-0 text-center select-category" id="tooltip-container">
                      <input class="form-check-input type-select" for="edit" type="radio" id="type_id_{{$type->id}}" name="type_id" @if($category->type_id == $type->id) checked @endif value="{{$type->id}}">
                      <label for="type_id_{{$type->id}}" class="card-body p-0 mb-0">
