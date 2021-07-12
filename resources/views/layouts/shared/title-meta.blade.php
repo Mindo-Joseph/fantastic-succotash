22ca67468ac31d085d4ed951b7485fb084db08d2
Puneet <puneet.g@codebrewinnovations.com>
2021-07-12 17:06:17 +0530

    title meta tag change

--- resources/views/layouts/shared/title-meta.blade.php
+++ resources/views/layouts/shared/title-meta.blade.php
@@ -3,7 +3,6 @@
 <meta charset="utf-8" />
 <title>{{$title ?? ' '}} | <?=  ucfirst($client ? $client->company_name : '') ?? 'Royo' ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" /> -->
 <meta content="Coderthemes" name="author" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <!-- App favicon -->
