0f73c267c05a459c55b1a659353a02213e4f750d
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-07-14 18:50:50 +0530

    changes in vendor list

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -274,13 +274,10 @@ class OrderController extends Controller {
         $orders = OrderVendor::where('user_id', $user->id)->orderBy('id', 'DESC');
         switch ($type) {
             case 'active':
-                $orders->whereNotIn('order_status_option_id',[6,3]);
+                $orders->whereNotIn('order_status_option_id', [6,3]);
             break;
             case 'past':
-                $order_status_options = [6,3];
-                $orders->whereHas('status', function ($query) use($order_status_options) {
-                    $query->whereIn('order_status_option_id', $order_status_options);
-                });
+                $orders->whereIn('order_status_option_id', [6,3]);
             break;
             case 'schedule':
                 $order_status_options = [10];
