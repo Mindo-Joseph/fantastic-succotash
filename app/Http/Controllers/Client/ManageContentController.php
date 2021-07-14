794c029bdc441dafc2c4181a38bd0839608e849d
Puneet <puneet.g@codebrewinnovations.com>
2021-07-14 13:09:19 +0530

    wallet issue fixed

--- app/Http/Controllers/Client/ManageContentController.php
+++ app/Http/Controllers/Client/ManageContentController.php
@@ -3,30 +3,26 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Client\BaseController;
-use App\Models\Banner;
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\Promocode;
-use App\Models\User;
-use App\Models\Variant;
-use App\Models\Vendor;
+use App\Models\{Banner,Category,Product,Promocode,User,Variant,Vendor};
 
 class ManageContentController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function deleteAllSoftDeleted()
     {
-        $cat = Category::onlyTrashed()->forceDelete();
-        $ven = Vendor::where('status', 2)->forceDelete();
-        $pro = Product::onlyTrashed()->forceDelete();
-        $promo_codes = Promocode::where('is_deleted', 1)->forceDelete();
-        $user = User::where('status', 3)->forceDelete();
-        $banners = Banner::where('status', 2)->forceDelete();
-        $variants = Variant::where('status', 2)->forceDelete();
-        return response()->json(['success' => 'Cleaned Successfully']);
+        try {
+            \DB::beginTransaction();
+            $pro = Product::onlyTrashed()->forceDelete();
+            $cat = Category::onlyTrashed()->forceDelete();
+            $user = User::where('status', 3)->forceDelete();
+            $ven = Vendor::where('status', 2)->forceDelete();
+            $banners = Banner::where('status', 2)->forceDelete();
+            $variants = Variant::where('status', 2)->forceDelete();
+            $promo_codes = Promocode::where('is_deleted', 1)->forceDelete();
+            \DB::commit();
+            return response()->json(['success' => 'Cleaned Successfully']);
+        } catch (\PDOException $e) {
+            \DB::rollBack();
+            return response()->json(['error' => $e->getMessage()]);
+        }
     }
 }
