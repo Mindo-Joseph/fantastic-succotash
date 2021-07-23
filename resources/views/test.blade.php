68b8ef94402e9ab7f9d01e34269618bf482ee5eb
chander_codebrew <chandar@codebrewinnovations.com>
2021-07-23 14:40:52 +0530

    header mobile responsive

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,7 +3,7 @@
 
     <section class="cab-booking pt-0">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4850.865733603189!2d76.82393041076074!3d30.716149768967526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1627015845978!5m2!1sen!2sin" width="100%" height="100vh" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-        <div class="booking-experience">
+        <div class="booking-experience ds">
             <div class="address-bar">
                 <ul>
                     <li class="pickup-text">
