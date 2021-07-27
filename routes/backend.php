82a3b5fe2d1674908de88df4ec1dba9ab8bd928d
Puneet <puneet.g@codebrewinnovations.com>
2021-07-27 14:51:29 +0530

    table boooking edit

--- routes/backend.php
+++ routes/backend.php
@@ -110,6 +110,9 @@ Route::group(['middleware' => ['ClientAuth','database'], 'prefix' => '/client'],
     Route::post('vendor/saveConfig/{id}', 'Client\VendorController@updateConfig')->name('vendor.config.update');
     Route::post('vendor/activeCategory/{id}', 'Client\VendorController@activeCategory')->name('vendor.category.update');
     Route::post('vendor/addCategory/{id}', 'Client\TableBookingController@storeCategory')->name('vendor.addCategory');
+    Route::post('vendor/updateCategory/{id}', 'Client\TableBookingController@updateCategory')->name('vendor.updateCategory');
+    Route::get('vendor/table/category/edit', 'Client\TableBookingController@editCategory')->name('vendor_table_category_edit');
+    Route::get('vendor/table/number/edit', 'Client\TableBookingController@editTable')->name('vendor_table_edit');
     Route::post('vendor/category/delete/{id}', 'Client\TableBookingController@destroyCategory')->name('vendor.category.delete');
     Route::post('vendor/addTable/{id}', 'Client\TableBookingController@storeTable')->name('vendor.addTable');
     Route::post('vendor/table/delete/{id}', 'Client\TableBookingController@destroyTable')->name('vendor.table.delete');
