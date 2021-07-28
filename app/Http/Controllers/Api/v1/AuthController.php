bd420cd0e57c3e0d200a3796d56e77522cf72843
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-07-28 11:31:32 +0530

    changes in verify email

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Api\v1\BaseController;
 use App\Http\Requests\{LoginRequest, SignupRequest};
-use App\Models\{User, Client, ClientPreference, BlockedToken, Otp, Country, UserDevice, UserVerification, ClientLanguage, CartProduct, Cart, UserRefferal};
+use App\Models\{User, Client, ClientPreference, BlockedToken, Otp, Country, UserDevice, UserVerification, ClientLanguage, CartProduct, Cart, UserRefferal, EmailTemplate};
 
 class AuthController extends BaseController{
     /**
@@ -306,52 +306,57 @@ class AuthController extends BaseController{
         $client = Client::select('id', 'name', 'email', 'phone_number', 'logo')->where('id', '>', 0)->first();
         $data = ClientPreference::select('sms_key', 'sms_secret', 'sms_from','mail_type', 'mail_driver', 'mail_host', 'mail_port', 'mail_username', 'sms_provider', 'mail_password', 'mail_encryption', 'mail_from')->where('id', '>', 0)->first();
         $newDateTime = \Carbon\Carbon::now()->addMinutes(10)->toDateTimeString();
-        if($user->is_phone_verified == 0){
-            $otp = mt_rand(100000, 999999);
-            $user->phone_token = $otp;
-            $user->phone_token_valid_till = $newDateTime;
-            $provider = $data->sms_provider;
-            $to = '+'.$request->dial_code.$request->phone_number;
-            $body = "Dear ".ucwords($user->name).", Please enter OTP ".$otp." to verify your account.";
-            if(!empty($data->sms_key) && !empty($data->sms_secret) && !empty($data->sms_from)){
-                $send = $this->sendSms($provider, $data->sms_key, $data->sms_secret, $data->sms_from, $to, $body);
-                if($send){
-                    $notified = 1;
+        if ($request->type == "phone") {
+            if($user->is_phone_verified == 0){
+                $otp = mt_rand(100000, 999999);
+                $user->phone_token = $otp;
+                $user->phone_token_valid_till = $newDateTime;
+                $provider = $data->sms_provider;
+                $to = '+917520822618';
+                $body = "Dear ".ucwords($user->name).", Please enter OTP ".$otp." to verify your account.";
+                if(!empty($data->sms_key) && !empty($data->sms_secret) && !empty($data->sms_from)){
+                    $send = $this->sendSms($provider, $data->sms_key, $data->sms_secret, $data->sms_from, $to, $body);
+                    if($send){
+                        $message = __('An otp has been sent to your phone. Please check.');
+                    }
                 }
             }
-        }
-        if($user->is_email_verified == 0){
+        }else{
+           if($user->is_email_verified == 0){
             $otp = mt_rand(100000, 999999);
             $user->email_token = $otp;
             $user->email_token_valid_till = $newDateTime;
-            if(!empty($data->mail_driver) && !empty($data->mail_host) && !empty($data->mail_port) && !empty($data->mail_port) && !empty($data->mail_password) && !empty($data->mail_encryption)){
-
-                $confirured = $this->setMailDetail($data->mail_driver, $data->mail_host, $data->mail_port, $data->mail_username, $data->mail_password, $data->mail_encryption);
-                $client_name = $client->name;
-                $mail_from = $data->mail_from;
-                $sendto = $user->email;
-                try{
-                    Mail::send('email.verify',[
-                            'customer_name' => ucwords($user->name),
-                            'code_text' => 'Enter below code to verify yoour account',
-                            'code' => $otp,
-                            'logo' => $client->logo['original'],
-                            'link'=>"link"
-                        ],
-                        function ($message) use($sendto, $client_name, $mail_from) {
-                        $message->from($mail_from, $client_name);
-                        $message->to($sendto)->subject('OTP to verify account');
-                    });
-                    $notified = 1;
+                if(!empty($data->mail_driver) && !empty($data->mail_host) && !empty($data->mail_port) && !empty($data->mail_port) && !empty($data->mail_password) && !empty($data->mail_encryption)){
+                    $confirured = $this->setMailDetail($data->mail_driver, $data->mail_host, $data->mail_port, $data->mail_username, $data->mail_password, $data->mail_encryption);
+                    $client_name = $client->name;
+                    $mail_from = $data->mail_from;
+                    $sendto = $user->email;
+                    $email_template_content = '';
+                    $email_template = EmailTemplate::where('id', 2)->first();
+                    if($email_template){
+                        $email_template_content = $email_template->content;
+                        $email_template_content = str_ireplace("{code}", $otp, $email_template_content);
+                        $email_template_content = str_ireplace("{customer_name}", ucwords($user->name), $email_template_content);
+                    }
+                    $data = [
+                        'code' => $otp,
+                        'link' => "link",
+                        'mail_from' => $mail_from,
+                        'email' => $request->email,
+                        'client_name' =>  $client->name,
+                        'logo' => $client->logo['original'],
+                        'subject' => $email_template->subject,
+                        'customer_name' => ucwords($user->name),
+                        'email_template_content' => $email_template_content,
+                    ];
+                    dispatch(new \App\Jobs\SendVerifyEmailJob($data))->onQueue('verify_email');
+                    $message = __('An otp has been sent to your email. Please check.');
                 }
-                catch(\Exception $e){
-                    $user->save();
-                }
-            }
+            } 
         }
         $user->save();
         if($notified == 1){
-            return response()->json(['success' => 'An otp has been sent to your email. Please check.'], 200); 
+            return response()->json(['success' => $message], 200); 
         }else{
             return response()->json(['success' => 'Provider service is not configured. Please contact administration.'], 404); 
         }
