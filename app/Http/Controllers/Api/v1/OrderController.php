b379804d4a0eda1697710cc5f70210705e80bbf0
Pankaj Rana <pankaj.pundir@codebrewinnovations.com>
2021-07-30 17:41:42 +0530

    changes in order controller

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -215,7 +215,7 @@ class OrderController extends Controller {
                         $order_vendor->coupon_id = $coupon_id;
                         $order_vendor->coupon_code = $coupon_name;
                         $order_vendor->order_status_option_id = 1;
-                        $OrderVendor->delivery_fee = $delivery_fee;
+                        $order_vendor->delivery_fee = $delivery_fee;
                         $order_vendor->subtotal_amount = $actual_amount;
                         $OrderVendor->payable_amount = $vendor_payable_amount + $delivery_fee;
                         $order_vendor->taxable_amount = $vendor_taxable_amount;
