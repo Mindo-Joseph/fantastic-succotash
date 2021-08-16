50c3046fa256a356bf9bcfbe4109c03917a85413
Puneet <puneet.g@codebrewinnovations.com>
2021-08-16 14:29:12 +0530

    node implementation

--- resources/views/backend/order/index.blade.php
+++ resources/views/backend/order/index.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.vertical', ['title' => 'Orders'])
-@section('content') 
+@section('content')
 <style type="text/css">
-    .ellipsis{
+    .ellipsis {
         white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
-    body{
+
+    body {
         font-size: 0.75rem;
     }
-    .order_data > div,.order_head h4 {
+
+    .order_data>div,
+    .order_head h4 {
         padding: 0 !important;
     }
+
     .order-page .card-box {
         padding: 20px 20px 5px !important;
     }
@@ -168,54 +172,58 @@
             <div class="page-title-box d-flex align-items-center justify-content-between">
                 <h4 class="page-title">Orders</h4>
                 <a class="return-btn" href="{{route('backend.order.returns',['Pending'])}}">
-                    <b>Return Request  <sup class="total-items">({{$return_requests}})</sup>
+                    <b>Return Request <sup class="total-items">({{$return_requests}})</sup>
                         <i class="fa fa-arrow-circle-right ml-1" aria-hidden="true"></i>
                     </b>
                 </a>
             </div>
         </div>
-    </div>    
     </div>
-    <script type="text/template" id="no_order_template">
-        <div class="error-msg"><p>You have not any order yet now.</p></div>
+</div>
+<script type="text/template" id="no_order_template">
+    <div class="error-msg"><p>You have not any order yet now.</p></div>
     </script>
-    <div class="loader" id="order_list_order">
-        <div class="spinner-border avatar-lg text-primary m-2" role="status"></div>
-    </div>  
-    <div class="row">
-        <div class="col-sm-12 col-lg-12 tab-product pt-0">
-            <ul class="nav nav-tabs nav-material" id="top-tab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="pending_order-tab" data-toggle="tab" href="#pending_orders" role="tab" aria-selected="false" data-rel="pending_orders">
-                        <i class="icofont icofont-man-in-glasses"></i>Pending Orders <sup class="total-items" id="pending-orders">({{$pending_order_count}})</sup>
-                    </a>
-                    <div class="material-border"></div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="active_orders_tab" data-toggle="tab"
-                        href="#active_orders" role="tab" aria-selected="true" data-rel="active_orders">
-                        <i class="icofont icofont-ui-home"></i>Active Orders <sup class="total-items" id="active-orders">({{$active_order_count}})</sup>
-                    </a>
-                    <div class="material-border"></div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="orders_history_tab" data-toggle="tab" href="#orders_history" role="tab" aria-selected="false" data-rel="orders_history">
-                        <i class="icofont icofont-man-in-glasses"></i>Orders History <sup class="total-items"  id="history-orders">({{$past_order_count}})</sup>
-                    </a>
-                    <div class="material-border"></div>
-                </li>
-            </ul>
-            <div class="tab-content nav-material  order_data_box scroll-style" id="top-tabContent">
-                <div class="tab-pane fade past-order show active" id="pending_orders" role="tabpanel"
-                    aria-labelledby="pending_order-tab"></div>
-                <div class="tab-pane fade" id="active_orders" role="tabpanel"
-                    aria-labelledby="active_orders_tab"></div>
-                <div class="tab-pane fade past-order" id="orders_history" role="tabpanel"
-                    aria-labelledby="orders_history_tab"><div class="error-msg"><p>You have not any order yet now.</p></div></div>
+<div class="loader" id="order_list_order">
+    <div class="spinner-border avatar-lg text-primary m-2" role="status"></div>
+</div>
+<div class="row">
+    <div class="col-sm-12 col-lg-12 tab-product pt-0">
+        <ul class="nav nav-tabs nav-material" id="top-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="pending_order-tab" data-toggle="tab" href="#pending_orders" role="tab" aria-selected="false" data-rel="pending_orders">
+                    <i class="icofont icofont-man-in-glasses"></i>Pending Orders <sup class="total-items" id="pending-orders">({{$pending_order_count}})</sup>
+                </a>
+                <div class="material-border"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="active_orders_tab" data-toggle="tab" href="#active_orders" role="tab" aria-selected="true" data-rel="active_orders">
+                    <i class="icofont icofont-ui-home"></i>Active Orders <sup class="total-items" id="active-orders">({{$active_order_count}})</sup>
+                </a>
+                <div class="material-border"></div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="orders_history_tab" data-toggle="tab" href="#orders_history" role="tab" aria-selected="false" data-rel="orders_history">
+                    <i class="icofont icofont-man-in-glasses"></i>Orders History <sup class="total-items" id="history-orders">({{$past_order_count}})</sup>
+                </a>
+                <div class="material-border"></div>
+            </li>
+        </ul>
+        <div class="tab-content nav-material  order_data_box scroll-style" id="top-tabContent">
+            <div class="tab-pane fade past-order show active" id="pending_orders" role="tabpanel" aria-labelledby="pending_order-tab"></div>
+            <div class="tab-pane fade" id="active_orders" role="tabpanel" aria-labelledby="active_orders_tab"></div>
+            <div class="tab-pane fade past-order" id="orders_history" role="tabpanel" aria-labelledby="orders_history_tab">
+                <div class="error-msg">
+                    <p>You have not any order yet now.</p>
+                </div>
             </div>
         </div>
-    </div>   
+    </div>
 </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.socket.io/4.1.2/socket.io.min.js" integrity="sha384-toS6mmwu70G0fw54EGlWWeA4z3dyJ+dlXBtSURSKN4vyRFOcxd3Bzjj/AoOwY+Rg" crossorigin="anonymous"></script>
+
 <script type="text/javascript">
     $(document).ready(function() {
         $.ajaxSetup({
@@ -223,43 +231,81 @@
                 'X-CSRF-TOKEN': $('input[name="_token"]').val()
             }
         });
-        setTimeout(function(){ $("#pending_order-tab").trigger('click'); }, 500);
-        $(document).on("click", ".load-more-btn",function() {
+        setTimeout(function() {
+            $("#pending_order-tab").trigger('click');
+        }, 500);
+        $(document).on("click", ".load-more-btn", function() {
             $('#order_list_order').show();
             var url = $(this).data('url');
             var rel = $(this).data('rel');
             init(rel, url);
             $(this).remove();
         });
-        $(".nav-link").click(function(){
+        $(".nav-link").click(function() {
             $('#order_list_order').show();
             var rel = $(this).data('rel');
             var url = "{{ route('orders.filter') }}";
             $(".tab-pane").html('');
             init(rel, url);
         });
-        function init(filter_order_status, url){
+        $(function() {
+            var url = window.location.href;
+            var arr = url.split("/");
+            var result = arr[2];
+            let ip_address = result;
+            let socket_port = "3100";
+            let socket = io(ip_address + ':' + socket_port);
+
+            // let chatInput = $('#chatInput');
+
+            // chatInput.keypress(function(e) {
+            //     let message = $(this).html();
+            //     console.log(message);
+            //     if(e.which === 13 && !e.shiftKey) {
+            //         socket.emit('sendChatToServer', message);
+            //         chatInput.html('');
+            //         return false;
+            //     }
+            // });
+
+            socket.on('sendChatToClient', (message) => {
+                $('#order_list_order').show();
+                var rel = "pending_orders";
+                var url = "{{ route('orders.filter') }}";
+                $(".tab-pane").html('');
+                init(rel, url);
+                // location.reload();
+            });
+        });
+
+        function init(filter_order_status, url) {
             $.ajax({
                 url: url,
                 type: "POST",
                 dataType: "JSON",
-                data: {filter_order_status:filter_order_status},
+                data: {
+                    filter_order_status: filter_order_status
+                },
                 success: function(response) {
                     $('#order_list_order').hide();
-                    if(response.status == 'Success'){
-                        if(response.data.orders.data.length != 0){
+                    if (response.status == 'Success') {
+                        if (response.data.orders.data.length != 0) {
                             let order_page_template = _.template($('#order_page_template').html());
-                            $("#"+filter_order_status).append(order_page_template({orders: response.data.orders.data, next_page_url:response.data.orders.next_page_url , filter_order_status:filter_order_status}));
-                        }else{
+                            $("#" + filter_order_status).append(order_page_template({
+                                orders: response.data.orders.data,
+                                next_page_url: response.data.orders.next_page_url,
+                                filter_order_status: filter_order_status
+                            }));
+                        } else {
                             let no_order_template = _.template($('#no_order_template').html());
-                            $("#"+filter_order_status).html(no_order_template({}));
+                            $("#" + filter_order_status).html(no_order_template({}));
                         }
-                        $("#active-orders").html("("+response.data.active_orders+")");
-                        $("#pending-orders").html("("+response.data.pending_orders+")");
-                        $("#history-orders").html("("+response.data.orders_history+")");
+                        $("#active-orders").html("(" + response.data.active_orders + ")");
+                        $("#pending-orders").html("(" + response.data.pending_orders + ")");
+                        $("#history-orders").html("(" + response.data.orders_history + ")");
                     }
                 },
-                error: function (data) {
+                error: function(data) {
 
                 },
             });
@@ -267,60 +313,60 @@
 
 
         // update status 
-        $(document).on("click", ".update-status",function() {
-         if(confirm("Are you Sure?")){
-            let that = $(this);
-            var count = that.data("count");
-            var full_div = that.data("full_div");
-            var single_div = that.data("single_div");
-            var status_option_id = that.data("status_option_id");
-            var status_option_id_next = status_option_id + 1;
-            var order_vendor_id = that.data("order_vendor_id");
-            var order_id = that.data("order_id");
-            var vendor_id = that.data("vendor_id");
-            var count = that.data("count");
-    
-            $.ajax({
-                url: "{{ route('order.changeStatus') }}",
-                type: "POST",
-                data: {
-                    order_id: order_id,
-                    vendor_id: vendor_id,
-                    "_token": "{{ csrf_token() }}",
-                    status_option_id: status_option_id,
-                    order_vendor_id: order_vendor_id,
-                },
-                success: function(response) {
-                      
-                    if(status_option_id == 4 || status_option_id == 5){
-                        if(status_option_id == 4)
-                        var next_status = 'Out For Delivery';
-                        else
-                        var next_status = 'Delivered';
-                        that.replaceWith("<button class='update-status btn-warning' data-full_div='"+ full_div +"' data-single_div='"+ single_div +"'  data-count='"+ count +"'  data-order_id='"+ order_id +"'  data-vendor_id='"+ vendor_id +"'  data-status_option_id='"+ status_option_id_next +"' data-order_vendor_id="+ order_vendor_id +">"+ next_status +"</button>");
-                        return false;
-                    }else{
-                        if(count == 0){
-                        $(full_div).slideUp(1000, function() {
-                        $(this).remove();
-                        });
-                
-                        }else{
-                        $(single_div).slideUp(1000, function() {
-                        $(this).remove();
-                        });
-                
+        $(document).on("click", ".update-status", function() {
+            if (confirm("Are you Sure?")) {
+                let that = $(this);
+                var count = that.data("count");
+                var full_div = that.data("full_div");
+                var single_div = that.data("single_div");
+                var status_option_id = that.data("status_option_id");
+                var status_option_id_next = status_option_id + 1;
+                var order_vendor_id = that.data("order_vendor_id");
+                var order_id = that.data("order_id");
+                var vendor_id = that.data("vendor_id");
+                var count = that.data("count");
+
+                $.ajax({
+                    url: "{{ route('order.changeStatus') }}",
+                    type: "POST",
+                    data: {
+                        order_id: order_id,
+                        vendor_id: vendor_id,
+                        "_token": "{{ csrf_token() }}",
+                        status_option_id: status_option_id,
+                        order_vendor_id: order_vendor_id,
+                    },
+                    success: function(response) {
+
+                        if (status_option_id == 4 || status_option_id == 5) {
+                            if (status_option_id == 4)
+                                var next_status = 'Out For Delivery';
+                            else
+                                var next_status = 'Delivered';
+                            that.replaceWith("<button class='update-status btn-warning' data-full_div='" + full_div + "' data-single_div='" + single_div + "'  data-count='" + count + "'  data-order_id='" + order_id + "'  data-vendor_id='" + vendor_id + "'  data-status_option_id='" + status_option_id_next + "' data-order_vendor_id=" + order_vendor_id + ">" + next_status + "</button>");
+                            return false;
+                        } else {
+                            if (count == 0) {
+                                $(full_div).slideUp(1000, function() {
+                                    $(this).remove();
+                                });
+
+                            } else {
+                                $(single_div).slideUp(1000, function() {
+                                    $(this).remove();
+                                });
+
+                            }
                         }
-                    }
 
-                    
 
-                    if(status_option_id == 2)
-                    $.NotificationApp.send("Success", response.message, "top-right", "#5ba035", "success");
-                   // location.reload();
-                },
-            });
-        }
+
+                        if (status_option_id == 2)
+                            $.NotificationApp.send("Success", response.message, "top-right", "#5ba035", "success");
+                        // location.reload();
+                    },
+                });
+            }
         });
     });
 </script>
