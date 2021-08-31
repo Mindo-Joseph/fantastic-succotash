f3ce1b05fe4007adf6eeefeb3b58d031d06c75b3
Ankush Garg <ankush.garg@codebrewinnovations.com>
2021-08-31 11:26:33 +0530

    Remove description from product detail screen

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -319,7 +319,7 @@
                             </div>
                         </div>
                     </div>
-                    <section class="tab-product m-0">
+                 {{--   <section class="tab-product m-0">
                         <div class="row">
                             <div class="col-sm-12 col-lg-12">
                                 <ul class="nav nav-tabs nav-material" id="top-tab" role="tablist">
@@ -383,7 +383,7 @@
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </section>--}}
                 </div>
             </div>
         </div>
