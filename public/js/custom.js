3976d2c1dbd38799737000e84f392d42cef58234
Ankush Garg <ankush.garg@codebrewinnovations.com>
2021-09-10 17:45:35 +0530

    stop redirecting for testing

--- public/js/custom.js
+++ public/js/custom.js
@@ -836,7 +836,7 @@ $(document).ready(function () {
                     socket.emit("createOrder", response.data );
                     setTimeout(function(){
                         window.location.href = base_url + "/order/success/" + response.data.id;
-                    },1000)
+                    },500)
                 } else {
                     if ($(".cart_response").length > 0) {
                         $(".cart_response").removeClass("d-none");
