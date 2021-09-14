2b3d2fcf87db405f284c90a93dc963ef35dfa200
dineshk123 <dineshk@codebrewinnovations.com>
2021-09-14 18:56:54 +0530

    cab booking

--- resources/views/frontend/booking/cabbooking-single-module.blade.php
+++ resources/views/frontend/booking/cabbooking-single-module.blade.php
@@ -12,9 +12,9 @@
                                 <input type="hidden" name="pickup_location_latitude" value="" id="destination_location_latitude_home_1" data-rel="1"/>
                                 <input type="hidden" name="pickup_location_longitude" value="" id="destination_location_longitude_home_1" data-rel="1"/>
                           
-                                <a class="location-btn" href="#">
+                                {{-- <a class="location-btn" href="#">
                                     <img src="{{asset('front-assets/images/arrow.svg')}}" alt="">
-                                </a>
+                                </a> --}}
                             </div>
                             <div class="form-group mb-0">
                                 <input class="form-control edit-other-stop" type="text" name="destination_location" placeholder="Enter drop location" id="destination_location_2" data-rel="2" >
