f1a2017efd2437ecffc1de93bbc34f956402ba3d
dineshk123 <dineshk@codebrewinnovations.com>
2021-09-16 19:28:28 +0530

    merge

--- database/seeders/HomePageLabelSeeder.php
+++ database/seeders/HomePageLabelSeeder.php
@@ -80,7 +80,7 @@ class HomePageLabelSeeder extends Seeder
         if($already == 0){
             $del = HomePageLabel::where('slug', 'cab_booking')->delete();
             $home_page = HomePageLabel::insertGetId([
-                'id' => 8,
+                'id' => 7,
                 'title' => 'Picup Delivery',
                 'slug' => 'pickup_delivery',
                 'order_by' => 7,
@@ -90,6 +90,14 @@ class HomePageLabelSeeder extends Seeder
         }
         else{
             $del = HomePageLabel::where('slug', 'cab_booking')->delete();
+            $del = HomePageLabel::where('slug', 'pickup_delivery')->delete();
+            $home_page = HomePageLabel::insertGetId([
+                'id' => 7,
+                'title' => 'Picup Delivery',
+                'slug' => 'pickup_delivery',
+                'order_by' => 7,
+                'is_active' => 0
+            ]);
            
         }   
        
