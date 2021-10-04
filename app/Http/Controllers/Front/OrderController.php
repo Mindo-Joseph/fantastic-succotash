fb7e4eaf2ff35c7a3d8bb38cb6bdaf16ef7a514f
Ankush Garg <ankush.garg@codebrewinnovations.com>
2021-10-04 18:06:31 +0530

    cron log and change android_channel_id

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -854,7 +854,7 @@ class OrderController extends FrontController
                         'sound' => "notification.wav",
                         "icon" => (!empty($client_preferences->favicon)) ? $client_preferences->favicon['proxy_url'] . '200/200' . $client_preferences->favicon['image_path'] : '',
                         'click_action' => route('order.index'),
-                        "android_channel_id" => "high-priority"
+                        "android_channel_id" => "sound-channel-id"
                     ],
                     "data" => [
                         'title' => $notification_content->subject,
