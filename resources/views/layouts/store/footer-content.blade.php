4fc668f341d7cae49978ed5b911bf43b0be5d640
Manmeet <manmeet.k@codebrewinnovations.com>
2021-10-04 19:05:41 +0530

    fix

--- resources/views/layouts/store/footer-content.blade.php
+++ resources/views/layouts/store/footer-content.blade.php
@@ -23,18 +23,14 @@ $pages = \App\Models\Page::with(['translations' => function($q) {$q->where('lang
                         <div class="footer-contant">
                             <ul>
                                 @foreach($pages as $page)
-                                {{--@if($page->slug=='driver-registration'&& $last_mile!=false)
+
+                                @if($page->translations->first()->type_of_form == 2)
+                                @if($last_mile_common_set!=false)
                                 <li>
-                                    <a href="{{route('page/driver-registration')}}">{{$page->translations->first() ? $page->translations->first()->title : $page->primary->title}}</a>
+                                    <a href="{{route('extrapage',['slug' => $page->slug])}}">{{$page->translations->first() ? $page->translations->first()->title : $page->primary->title}}</a>
                                 </li>
-                                @else--}}
-                                @if($page->translation->type_of_form == 2)
-                                    @if($last_mile_common_set!=false)
-                                    <li>
-                                        <a href="{{route('extrapage',['slug' => $page->slug])}}">{{$page->translations->first() ? $page->translations->first()->title : $page->primary->title}}</a>
-                                    </li>
-                                    @endif
-                                @else    
+                                @endif
+                                @else
                                 <li>
                                     <a href="{{route('extrapage',['slug' => $page->slug])}}">{{$page->translations->first() ? $page->translations->first()->title : $page->primary->title}}</a>
                                 </li>
