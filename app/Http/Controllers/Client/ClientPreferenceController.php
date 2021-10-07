982cd0fbd667cf5cfdcad3fc2b13ba75cc29c5e6
dineshk123 <dineshk@codebrewinnovations.com>
2021-10-07 11:30:08 +0530

    custom domai

--- app/Http/Controllers/Client/ClientPreferenceController.php
+++ app/Http/Controllers/Client/ClientPreferenceController.php
@@ -395,25 +395,27 @@ class ClientPreferenceController extends BaseController{
                 }
 
                // $process = shell_exec("/var/app/Automation/script.sh '".$my_url."' ");
+               $exists = Client::on('god')->where('code',$id)->where('custom_domain', $request->custom_domain)->count();
+               if ($exists) {
+                   return redirect()->back()->withInput()->withErrors(new \Illuminate\Support\MessageBag(['custom_domain' => 'Domain name "' . $request->custom_domain . '" is not available. Please select a different domain']));
+               } else {
+                   Client::on('god')->where('code',$id)->update(['custom_domain' => $request->custom_domain]);
+                    $dbname = DB::connection()->getDatabaseName();
+                   if ($dbname != env('DB_DATABASE')) {
+                       Client::where('id', '!=', 0)->update(['custom_domain' => $request->custom_domain]);
+                   }
+               }
+               return redirect()->route('configure.customize')->with('success', 'Client customize data updated successfully!');
             } catch (Exception $e) {
                 return redirect()->back()->withInput()->withErrors(new \Illuminate\Support\MessageBag(['custom_domain' => $e->getMessage()]));
             }
           
-             $exists = Client::on('god')->where('code',$id)->where('custom_domain', $request->custom_domain)->count();
-            if ($exists) {
-                return redirect()->back()->withInput()->withErrors(new \Illuminate\Support\MessageBag(['custom_domain' => 'Domain name "' . $request->custom_domain . '" is not available. Please select a different domain']));
-            } else {
-                Client::on('god')->where('code',$id)->update(['custom_domain' => $request->custom_domain]);
-                 $dbname = DB::connection()->getDatabaseName();
-                if ($dbname != env('DB_DATABASE')) {
-                    Client::where('id', '!=', 0)->update(['custom_domain' => $request->custom_domain]);
-                }
-            }
+           
         }
 
 
 
 
-        return redirect()->route('configure.customize')->with('success', 'Client customize data updated successfully!');
+       
     }
 }
