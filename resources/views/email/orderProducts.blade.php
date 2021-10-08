f17d36ed19a687b87d5896847a6a55174c1d8bc6
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-10-08 16:13:36 +0530

    order email template related changes

--- resources/views/email/orderProducts.blade.php
+++ resources/views/email/orderProducts.blade.php
@@ -17,14 +17,14 @@
                  <td align="left">
                     <div style="padding:5px"><img src="{{$vendor_product['product']['media'][0]['image']['path']['image_fit']}}100/100{{$vendor_product['product']['media'][0]['image']['path']['image_path']}}" alt="" width="80"></div>
                  </td>
-                 <td valign="top" align="left" style="padding-left: 15px;">
-                     <h4 style="margin-top: 15px;">{{$vendor_product['product']['translation_one']['title']}}</h4>
+                 <td valign="top" align="left">
+                     <h4 style="margin-top: 10px;">{{$vendor_product['product']['translation_one']['title']}}</h4>
                  </td>
-                 <td valign="top" align="center" style="padding-left: 15px;">
-                     <h4 style="color:#444;margin-top: 10px;"><span>{{$vendor_product['quantity']}}</span></h4>
+                 <td valign="top" align="center">
+                     <h4 style="margin-top: 10px;"><span>{{$vendor_product['quantity']}}</span></h4>
                  </td>
-                 <td valign="top" align="right" style="padding-left: 15px;">
-                     <h4 style="color:#444;margin-top:15px"><b>{{ $currencySymbol . number_format($vendor_product['pvariant']['price'], 2, '.', '')}}</b></h4>
+                 <td valign="top" align="right">
+                     <h4 style="margin-top:10px"><b>{{ $currencySymbol . number_format($vendor_product['pvariant']['price'], 2, '.', '')}}</b></h4>
                      @php
                      $total_products += $vendor_product['pvariant']['price'];
                      @endphp
@@ -47,6 +47,7 @@
                      <b>{{$currencySymbol . $product['taxable_amount']}}</b>
                  </td>
              </tr>
+             @if($product['delivery_fee_charges'] > 0)
              <tr class="pad-left-right-space">
                  <td colspan="2" align="left">
                      <p style="font-size: 14px;">Shipping Charge :</p>
@@ -55,6 +56,8 @@
                      <b>{{$currencySymbol . $product['delivery_fee_charges']}}</b>
                  </td>
              </tr>
+             @endif
+             @if($product['discount_amount'] > 0)
              <tr class="pad-left-right-space">
                  <td colspan="2" align="left">
                      <p style="font-size: 14px;">Discount :</p>
@@ -63,6 +66,7 @@
                      <b>{{$currencySymbol . $product['discount_amount']}}</b>
                  </td>
              </tr>
+             @endif
              <tr class="pad-left-right-space ">
                  <td class="m-b-5" colspan="2" align="left">
                      <p style="font-size: 14px;">Total :</p>
