1e485b1197dc3fa789bf9aa0e209b6165a99f263
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-10-08 20:16:48 +0530

    dynamic email template html related changes

--- resources/views/email/orderSuccesseEmail.blade.php
+++ resources/views/email/orderSuccesseEmail.blade.php
@@ -66,11 +66,9 @@
 
         table {
             margin-top: 10px;
-            border-radius: 10px;
         }
         table th {
             font-size: 14px;
-            border-radius: 10px;
         }
 
         table.top-0 {
