7cbf4b86de7448eafa5d63436ddbb2ddbf68d9cf
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-10-23 14:05:48 +0530

    changes

--- app/Http/Controllers/Front/PaylinkGatewayController.php
+++ app/Http/Controllers/Front/PaylinkGatewayController.php
@@ -168,7 +168,6 @@ class PaylinkGatewayController extends FrontController
 
 
         if ($response->result->status == 'PAID') {
-
             // if ($request->succes == 'true') {
 
             if ($order) {
