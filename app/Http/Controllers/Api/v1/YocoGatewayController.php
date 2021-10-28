6bb5792c4b9d7f1205b0fc18b757243665b25346
root <root@ip-172-31-27-234.us-west-2.compute.internal>
2021-10-28 10:01:09 +0000

    merging files

--- app/Http/Controllers/Api/v1/YocoGatewayController.php
+++ app/Http/Controllers/Api/v1/YocoGatewayController.php
@@ -10,6 +10,7 @@ use Yoco\YocoClient;
 use Yoco\Exceptions\ApiKeyException;
 use Yoco\Exceptions\DeclinedException;
 use Yoco\Exceptions\InternalException;
+use Illuminate\Support\Facades\Redirect;
 use Omnipay\Omnipay;
 use Illuminate\Http\Request;
 use Omnipay\Common\CreditCard;
@@ -35,17 +36,11 @@ class YocoGatewayController extends BaseController
         $creds_arr = json_decode($yoco_creds->credentials);
         $secret_key = (isset($creds_arr->secret_key)) ? $creds_arr->secret_key : '';
         $public_key = (isset($creds_arr->public_key)) ? $creds_arr->public_key : '';
-        // $api_access_token = (isset($creds_arr->api_access_token)) ? $creds_arr->api_access_token : '';
+
         $this->test_mode = (isset($yoco_creds->test_mode) && ($yoco_creds->test_mode == '1')) ? true : false;
 
         $this->SECRET_KEY = $secret_key;
         $this->PUBLIC_KEY = $public_key;
-<<<<<<< HEAD
-        // $this->API_ACCESS_TOKEN = $api_access_token;
-
-
-=======
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
     }
 
     public function yocoPurchase(Request $request, $domain = '')
@@ -70,20 +65,6 @@ class YocoGatewayController extends BaseController
             $cart = Cart::select('id')->where('status', '0')->where('user_id', $user->id)->first();
             $amount = $this->getDollarCompareAmount($request->amount);
             $amount = filter_var($amount, FILTER_SANITIZE_NUMBER_INT);
-<<<<<<< HEAD
-     
-            // $returnUrlParams = '?amount='.$amount;
-            // if($request->has('tip')){
-            //     $tip = $request->tip;
-            //     $returnUrlParams = $returnUrlParams.'&tip='.$tip;
-            // }
-            // if( ($request->has('address_id')) && ($request->address_id > 0) ){
-            //     $address_id = $request->address_id;
-            //     $returnUrlParams = $returnUrlParams.'&address_id='.$address_id;
-            // }
-            $returnUrlParams = '?gateway=yoco&order=' . $request->order_number;
-=======
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
 
 
             // $returnUrlParams = '?gateway=yoco&order=' . $request->order_number;
@@ -98,30 +79,12 @@ class YocoGatewayController extends BaseController
                 'amountInCents' => $amount,
                 'currency' => 'ZAR',
                 'description' => 'Order Checkout',
-<<<<<<< HEAD
-                //'return_url' => url($request->returnUrl . $returnUrlParams),
-                'reference' => $request->order_number,
-                //'webhook' => url('/payment/yoco/notify/'),
-                'redirect' => false,
-                'test' => $this->test_mode, // True, testing, false, production
-                // 'options' => array(
-                //     'theme' => array(
-                //         'type' => 'light', // dark or light color scheme
-                //         'showHeader' => true,
-                //         'header' => array(
-                //             'name' => 'Your brand name',
-                //             'logo' => 'https://www.yourstore.com/store-logo.jpg', // Must be https!
-                //         ),
-                //     ),
-                // ),
-=======
 
                 'reference' => $request->order_number,
 
                 'redirect' => false,
                 'test' => $this->test_mode, // True, testing, false, production
 
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
                 'customer' => array(
                     'email' => $user->email,
                     'name' => $user->name,
@@ -130,16 +93,7 @@ class YocoGatewayController extends BaseController
                 )
             );
 
-<<<<<<< HEAD
-          //  $client = new YocoClient($this->SECRET_KEY, $this->PUBLIC_KEY);
-            // WebhookCall::create()
-            //     ->url('payment/yoco/notify')
-            //     ->payload($checkout_data)
-            //     ->useSecret($this->SECRET_KEY)
-            //     ->dispatch();
-=======
 
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
             $ch = curl_init();
 
             curl_setopt($ch, CURLOPT_URL, "https://online.yoco.com/v1/charges/");
@@ -158,21 +112,6 @@ class YocoGatewayController extends BaseController
             if ($result->status == 'successful') {
                 $this->yocoSuccess($request, $result);
                 // $response = $this->mb->mobbex_checkout($checkout_data);
-<<<<<<< HEAD
-                return $this->successResponse(url($returnUrl . $returnUrlParams));
-            }
-            else {
-                $this->yocoFail($request);
-                return $this->errorResponse($result->status, 400);
-            }
-            // if ($response['response']['result']) {
-            //     return $this->successResponse($response['response']['data']['url']);
-            // } elseif (!$response['response']['result']) {
-            //     return $this->errorResponse($response['response']['error'], 400);
-            // } else {
-            //     return $this->errorResponse($response->getMessage(), 400);
-            // }
-=======
                 return $this->successResponse(url( $request->serverUrl . 'payment/gateway/returnResponse?status=0&gateway=yoco&order=' . $request->order_number));
             } else {
                 $this->yocoFail($request);
@@ -192,7 +131,6 @@ class YocoGatewayController extends BaseController
             $amount = $this->getDollarCompareAmount($request->amount);
             $amount = filter_var($amount, FILTER_SANITIZE_NUMBER_INT);
 
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
 
             // $returnUrlParams = '?gateway=yoco&order=' . $request->order_number;
 
@@ -251,56 +189,6 @@ class YocoGatewayController extends BaseController
 
     public function yocoSuccess($request, $result, $domain = '')
     {
-<<<<<<< HEAD
-        // Notify Mobbex that information has been received
-        // header( 'HTTP/1.0 200 OK' );
-        // flush();
-       // Log::info('testing');
-
-  
-            $transactionId = $result->id;
-            $order_number = $request->order_number;
-            $order = Order::with(['paymentOption', 'user_vendor', 'vendors:id,order_id,vendor_id'])->where('order_number', $order_number)->first();
-            if ($order) {
-               
-                    $order->payment_status = 1;
-                    $order->save();
-                    $payment_exists = Payment::where('transaction_id', $transactionId)->first();
-                    if (!$payment_exists) {
-                        Payment::insert([
-                            'date' => date('Y-m-d'),
-                            'order_id' => $order->id,
-                            'transaction_id' => $transactionId,
-                            'balance_transaction' => $request->amount,
-                        ]);
-
-                        // Auto accept order
-                        $orderController = new OrderController();
-                        $orderController->autoAcceptOrderIfOn($order->id);
-
-                        // Remove cart
-                     
-                        Cart::where('id', $request->cart_id)->update(['schedule_type' => NULL, 'scheduled_date_time' => NULL]);
-                        CartAddon::where('cart_id', $request->cart_id)->delete();
-                        CartCoupon::where('cart_id', $request->cart_id)->delete();
-                        CartProduct::where('cart_id', $request->cart_id)->delete();
-                        CartProductPrescription::where('cart_id', $request->cart_id)->delete();
-
-                        // Send Notification
-                        if (!empty($order->vendors)) {
-                            foreach ($order->vendors as $vendor_value) {
-                                $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id, $vendor_value->vendor_id);
-                                $user_vendors = UserVendor::where(['vendor_id' => $vendor_value->vendor_id])->pluck('user_id');
-                                $orderController->sendOrderPushNotificationVendors($user_vendors, $vendor_order_detail);
-                            }
-                        }
-                        $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id);
-                        $super_admin = User::where('is_superadmin', 1)->pluck('id');
-                        $orderController->sendOrderPushNotificationVendors($super_admin, $vendor_order_detail);
-
-                        // Send Email
-                       
-=======
 
 
 
@@ -338,7 +226,6 @@ class YocoGatewayController extends BaseController
                         $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id, $vendor_value->vendor_id);
                         $user_vendors = UserVendor::where(['vendor_id' => $vendor_value->vendor_id])->pluck('user_id');
                         $orderController->sendOrderPushNotificationVendors($user_vendors, $vendor_order_detail);
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
                     }
                 }
                 $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id);
@@ -366,11 +253,7 @@ class YocoGatewayController extends BaseController
         OrderVendor::where('order_id', $order->id)->delete();
         OrderTax::where('order_id', $order->id)->delete();
         Order::where('id', $order->id)->delete();
-<<<<<<< HEAD
-    
-=======
         return Redirect::to(url('viewcart'));
->>>>>>> f70fa067f9634e170feb8f4839ad3bb2eea9e264
     }
 
   
