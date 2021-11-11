0287c9c863898b19fecc5d877fed3d8406f2085d
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-11-11 16:37:41 +0530

    changes in paylink gateway

--- app/Http/Controllers/Front/YocoGatewayController.php
+++ app/Http/Controllers/Front/YocoGatewayController.php
@@ -276,11 +276,9 @@ class YocoGatewayController extends FrontController
                     return $this->successResponse($result);
                 }
                 $this->yocoSuccessApp($request, $result);
-                Auth::logout();
                 return $this->successResponse($result);
             } else {
                 $this->yocoFailApp($request);
-                Auth::logout();
                 return $this->errorResponse($result->status, 400);
             }
         } catch (\Exception $ex) {
