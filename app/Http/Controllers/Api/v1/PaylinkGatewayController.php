87d93c9c023d058caf21d316e364d71a712e7c38
preet_inder <preetinder.pal@codebrewinnovations.com>
2021-11-11 12:12:45 +0530

    changes regarding pointcheckout gateway in app

--- app/Http/Controllers/Api/v1/PaylinkGatewayController.php
+++ app/Http/Controllers/Api/v1/PaylinkGatewayController.php
@@ -43,12 +43,7 @@ class PaylinkGatewayController extends BaseController
             $cart = Cart::select('id')->where('status', '0')->where('user_id', $user->id)->first();
             $amount = $this->getDollarCompareAmount($request->amount);
 
-            $returnUrlParams = '?gateway=paylink&order=' . $request->order_number;
-
-            $returnUrl = route('order.return.success');
-            if ($request->payment_form == 'wallet') {
-                $returnUrl = route('user.wallet');
-            }
+            $request->request->add(['payment_form' => $request->action]);
             $uniqid = uniqid();
             $customer_data = array(
                 'firstName' => $user->name,
@@ -58,15 +53,15 @@ class PaylinkGatewayController extends BaseController
                 // 'identification' => '12123123'
             );
             $reference_number = $description = '';
+            $returnUrlParams = '?gateway=paylink&amount=' . $request->amount . '&payment_form=' . $request->payment_form;
 
             if($request->payment_form == 'cart'){
                 $description = 'Order Checkout';
                 $cart = Cart::select('id')->where('status', '0')->where('user_id', $user->id)->first();
                 $request->request->add(['cart_id' => $cart->id]);
                 $customer_data['cart_id'] = $cart->id;
-                if($request->has('order_number')){
-                    $reference_number = $request->order_number;
-                }
+                $reference_number = $request->order_number;
+                $returnUrlParams = $returnUrlParams . '&cart_id=' . $cart->id . '&order=' . $request->order_number;
             }
             elseif($request->payment_form == 'wallet'){
                 $description = 'Wallet Checkout';
@@ -89,8 +84,6 @@ class PaylinkGatewayController extends BaseController
                 }
             }
 
-            $returnUrlParams = '?gateway=paylink&amount=' . $request->amount . '&cart_id=' . $request->cart_id . '&order=' . $request->order_number . '&payment_form=' . $request->payment_form;
-
             $data = array(
                 'requestId' => 'CHK-' . $uniqid,
                 'orderId' => $reference_number,
@@ -98,7 +91,7 @@ class PaylinkGatewayController extends BaseController
                 'currency' => 'AED', //$this->currency
                 'description' => $description,
                 'reference' => $reference_number,
-                'returnUrl' => url('payment/paylink/return' . $returnUrlParams),
+                'returnUrl' => url('payment/paylink/return/app' . $returnUrlParams),
                 'redirect' => false,
                 'test' => $this->test_mode, // True, testing, false, production
                 'customer' => $customer_data,
@@ -146,104 +139,6 @@ class PaylinkGatewayController extends BaseController
             }
         } catch (\Exception $ex) {
             return $this->errorResponse($ex->getMessage(), 400);
-        }
-    }
-
-    public function paylinkReturn(Request $request, $domain = '')
-    {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->getCheckoutUrl() . '/' . $request->checkout,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-                'X-PointCheckout-Api-Key:' . $this->API_KEY,
-                'X-PointCheckout-Api-Secret:' . $this->API_SECRET_KEY,
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-        $response = json_decode($response);
-
-        //  dd($response);
-        $transactionId = $request->checkout;
-
-        if ($response->result->status == 'PAID') {
-            if($request->payment_form == 'cart'){
-                $order_number = $request->order;
-                $order = Order::with(['paymentOption', 'user_vendor', 'vendors:id,order_id,vendor_id'])->where('order_number', $order_number)->first();
-                if ($order) {
-                    $order->payment_status = 1;
-                    $order->save();
-                    $payment_exists = Payment::where('transaction_id', $transactionId)->first();
-                    if (!$payment_exists) {
-                        Payment::insert([
-                            'date' => date('Y-m-d'),
-                            'order_id' => $order->id,
-                            'transaction_id' => $transactionId,
-                            'balance_transaction' => $request->amount,
-                        ]);
-
-                        // Auto accept order
-                        $orderController = new OrderController();
-                        $orderController->autoAcceptOrderIfOn($order->id);
-
-                        // Remove cart
-                        Cart::where('id', $request->cart_id)->update(['schedule_type' => null, 'scheduled_date_time' => null]);
-                        CartAddon::where('cart_id', $request->cart_id)->delete();
-                        CartCoupon::where('cart_id', $request->cart_id)->delete();
-                        CartProduct::where('cart_id', $request->cart_id)->delete();
-                        CartProductPrescription::where('cart_id', $request->cart_id)->delete();
-
-                        // Send Notification
-                        if (!empty($order->vendors)) {
-                            foreach ($order->vendors as $vendor_value) {
-                                $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id, $vendor_value->vendor_id);
-                                $user_vendors = UserVendor::where(['vendor_id' => $vendor_value->vendor_id])->pluck('user_id');
-                                $orderController->sendOrderPushNotificationVendors($user_vendors, $vendor_order_detail);
-                            }
-                        }
-                        $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id);
-                        $super_admin = User::where('is_superadmin', 1)->pluck('id');
-                        $orderController->sendOrderPushNotificationVendors($super_admin, $vendor_order_detail);
-                    }
-                    $returnUrlParams = ''; //'?gateway=paylink&order=' . $order->id;
-                    $returnUrl = route('order.return.success');
-                    return Redirect::to(url($returnUrl . $returnUrlParams));
-
-                    // Send Email
-                    //   $this->successMail();
-                }
-            } elseif($request->payment_form == 'wallet'){
-                $returnUrlParams = '';//'?gateway=paylink&amount=' . $request->amount . '&checkout=' . $request->checkout ;
-                $returnUrl = route('user.wallet');
-                return Redirect::to(url($returnUrl . $returnUrlParams));
-            }
-            return Redirect::to(route('order.return.success'));
-        } 
-        else {
-            if($request->payment_form == 'cart'){
-                $order_number = $request->order;
-                $order = Order::with(['paymentOption', 'user_vendor', 'vendors:id,order_id,vendor_id'])->where('order_number', $order_number)->first();
-                $order_products = OrderProduct::select('id')->where('order_id', $order->id)->get();
-                foreach ($order_products as $order_prod) {
-                    OrderProductAddon::where('order_product_id', $order_prod->id)->delete();
-                }
-                OrderProduct::where('order_id', $order->id)->delete();
-                OrderProductPrescription::where('order_id', $order->id)->delete();
-                VendorOrderStatus::where('order_id', $order->id)->delete();
-                OrderVendor::where('order_id', $order->id)->delete();
-                OrderTax::where('order_id', $order->id)->delete();
-                Order::where('id', $order->id)->delete();
-                return Redirect::to(route('showCart'));
-            }
         }
     }
 
--- app/Http/Controllers/Front/PaylinkGatewayController.php
+++ app/Http/Controllers/Front/PaylinkGatewayController.php
@@ -250,6 +250,102 @@ class PaylinkGatewayController extends FrontController
         }
     }
 
+    public function paylinkReturnApp(Request $request)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->getCheckoutUrl() . '/' . $request->checkout,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'X-PointCheckout-Api-Key:' . $this->API_KEY,
+                'X-PointCheckout-Api-Secret:' . $this->API_SECRET_KEY,
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response = json_decode($response);
+
+        //  dd($response);
+        $transactionId = $request->checkout;
+        $returnUrl = url('payment/gateway/returnResponse');
+
+        if ($response->result->status == 'PAID') {
+            if($request->payment_form == 'cart'){
+                $order_number = $request->order;
+                $order = Order::with(['paymentOption', 'user_vendor', 'vendors:id,order_id,vendor_id'])->where('order_number', $order_number)->first();
+                if ($order) {
+                    $order->payment_status = 1;
+                    $order->save();
+                    $payment_exists = Payment::where('transaction_id', $transactionId)->first();
+                    if (!$payment_exists) {
+                        Payment::insert([
+                            'date' => date('Y-m-d'),
+                            'order_id' => $order->id,
+                            'transaction_id' => $transactionId,
+                            'balance_transaction' => $request->amount,
+                        ]);
+
+                        // Auto accept order
+                        $orderController = new OrderController();
+                        $orderController->autoAcceptOrderIfOn($order->id);
+
+                        // Remove cart
+                        Cart::where('id', $request->cart_id)->update(['schedule_type' => null, 'scheduled_date_time' => null]);
+                        CartAddon::where('cart_id', $request->cart_id)->delete();
+                        CartCoupon::where('cart_id', $request->cart_id)->delete();
+                        CartProduct::where('cart_id', $request->cart_id)->delete();
+                        CartProductPrescription::where('cart_id', $request->cart_id)->delete();
+
+                        // Send Notification
+                        if (!empty($order->vendors)) {
+                            foreach ($order->vendors as $vendor_value) {
+                                $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id, $vendor_value->vendor_id);
+                                $user_vendors = UserVendor::where(['vendor_id' => $vendor_value->vendor_id])->pluck('user_id');
+                                $orderController->sendOrderPushNotificationVendors($user_vendors, $vendor_order_detail);
+                            }
+                        }
+                        $vendor_order_detail = $orderController->minimize_orderDetails_for_notification($order->id);
+                        $super_admin = User::where('is_superadmin', 1)->pluck('id');
+                        $orderController->sendOrderPushNotificationVendors($super_admin, $vendor_order_detail);
+                    }
+
+                    // Send Email
+                    //   $this->successMail();
+                }
+            } elseif($request->payment_form == 'wallet'){
+                
+            }
+            $returnUrlParams = '?status=200&gateway=paylink&order=' . $order_number;
+            return Redirect::to(url($returnUrl . $returnUrlParams));
+        } 
+        else {
+            if($request->payment_form == 'cart'){
+                $order_number = $request->order;
+                $order = Order::with(['paymentOption', 'user_vendor', 'vendors:id,order_id,vendor_id'])->where('order_number', $order_number)->first();
+                $order_products = OrderProduct::select('id')->where('order_id', $order->id)->get();
+                foreach ($order_products as $order_prod) {
+                    OrderProductAddon::where('order_product_id', $order_prod->id)->delete();
+                }
+                OrderProduct::where('order_id', $order->id)->delete();
+                OrderProductPrescription::where('order_id', $order->id)->delete();
+                VendorOrderStatus::where('order_id', $order->id)->delete();
+                OrderVendor::where('order_id', $order->id)->delete();
+                OrderTax::where('order_id', $order->id)->delete();
+                Order::where('id', $order->id)->delete();
+                $returnUrlParams = '?status=0&gateway=paylink&order=' . $order_number;
+                return Redirect::to(url($returnUrl . $returnUrlParams));
+            }
+        }
+    }
+
     private function getCheckoutUrl(){
         if ($this->test_mode == true){
             return 'https://api.test.pointcheckout.com/mer/v2.0/checkout';
