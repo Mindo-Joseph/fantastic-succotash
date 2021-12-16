7228c69767cb00ccea5b71a155edcb304bbb3c1d
dineshk123 <dineshk@codebrewinnovations.com>
2021-12-16 17:30:23 +0530

    merge

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -317,7 +317,7 @@ class OrderController extends FrontController
                     $email_template_content = $email_template->content;
                     if ($vendor_id == "") {
                         $returnHTML = view('email.newOrderProducts')->with(['cartData' => $cartDetails, 'order' => $order, 'currencySymbol' => $currSymbol])->render();
-                        dd($returnHTML);
+                       return $returnHTML;
                     } else {
                         $returnHTML = view('email.orderVendorProducts')->with(['cartData' => $cartDetails, 'id' => $vendor_id, 'currencySymbol' => $currSymbol])->render();
                     }
@@ -961,8 +961,8 @@ class OrderController extends FrontController
             }
             $order->save();
             foreach ($cart_products->groupBy('vendor_id') as $vendor_id => $vendor_cart_products) {
-                $this->sendSuccessEmail($request, $order, $vendor_id);
-                dd($order);
+                $email_send = $this->sendSuccessEmail($request, $order, $vendor_id);
+                dd($email_send);
             }
             // $this->sendOrderNotification($user->id, $vendor_ids);
             $this->sendSuccessEmail($request, $order);
