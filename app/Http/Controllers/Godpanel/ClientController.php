272b6540ec8018267b2164fdae323c462490a7b2
dineshk123 <dineshk@codebrewinnovations.com>
2021-12-20 19:02:52 +0530

    merge

--- app/Http/Controllers/Godpanel/ClientController.php
+++ app/Http/Controllers/Godpanel/ClientController.php
@@ -460,15 +460,76 @@ class ClientController extends Controller{
 
      public function exportDb(Request $request,$databaseName){
 
-        $client = Client::where('database_name',$databaseName)->first();
+        $client = Client::where('database_name',$databaseName)->first(['name', 'email', 'password', 'phone_number', 'database_host','database_path', 'database_name', 'database_username', 'database_password', 'logo', 'company_name', 'company_address', 'custom_domain', 'status', 'code', 'country_id', 'sub_domain'])->toarray();
         $check_if_already = 0;
         $request->dump_into = 'DEV';
         $data = $request->all();
         if($client){
             
-            $check_if_already = Client::on($request->dump_into)->where(['database_name' => $client->database_name])->where(['sub_domain' => $client->sub_domain])->count();
+            $check_if_already = Client::on($request->dump_into)->where(['database_name' => $client['database_name']])->where(['sub_domain' => $client['sub_domain']])->count();
             if($check_if_already == 0){
-                $create_db = $this->dispatchNow(new ClientDatabaseToDevMaster($client->id,$request->dump_into));
+                $clientData = array();
+                try {
+
+                    $databaseNameSet = 'royo_'.$client['database_name'];
+                    $db_name_set = $databaseNameSet.'.sql';
+                    \Spatie\DbDumper\Databases\MySql::create()
+                        ->setDbName($databaseNameSet)
+                        ->setUserName($client['database_username'])
+                        ->setPassword($client['database_password'])
+                        ->setHost($client['database_host'])
+                        ->dumpToFile($db_name_set);
+         
+                    $schemaName = 'royo_' . $client['database_name'] ?: config("database.connections.mysql.database");
+                    $dumpinto = $request->dump_into;
+        
+                        $database_host_dev = env('DB_HOST_'.$dumpinto, '');
+                        $database_port_dev = env('DB_PORT_'.$dumpinto, '3306');
+                        $database_username_dev = env('DB_USERNAME_'.$dumpinto, '');
+                        $database_password_dev =  env('DB_PASSWORD_'.$dumpinto, '');
+        
+                        $default = [
+                        'driver' => env('DB_CONNECTION_'.$dumpinto, 'mysql'),
+                        'host' => $database_host_dev,
+                        'port' => $database_port_dev,
+                        'database' => $schemaName,
+                        'username' => $database_username_dev,
+                        'password' => $database_password_dev,
+                        'charset' => 'utf8mb4',
+                        'collation' => 'utf8mb4_unicode_ci',
+                        'prefix' => '',
+                        'prefix_indexes' => true,
+                        'strict' => false,
+                        'engine' => null
+                        ];
+        
+                        $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME =  ?";
+                        $db = DB::connection($dumpinto)->select($query, [$schemaName]);
+                        if ($db) {
+                            return redirect()->route('client.index')->with('error', 'Database already exist');
+                        }else{
+                            $query = "CREATE DATABASE $schemaName;";
+                            DB::connection($dumpinto)->statement($query);
+                        }
+        
+                        Config::set("database.connections.$schemaName", $default);
+                        config(["database.connections.mysql.database" => $schemaName]);
+                        DB::connection($schemaName)->beginTransaction();
+                        DB::connection($schemaName)->statement("SET foreign_key_checks=0");
+                        DB::connection($schemaName)->unprepared(file_get_contents((asset($db_name_set))));
+                        DB::connection($schemaName)->commit();
+                        DB::connection($schemaName)->statement("SET foreign_key_checks=1");
+                        DB::connection($schemaName)->table('clients')->where('database_name',$client['database_name'])->update(['database_host' => $database_host_dev]);
+                    
+                    DB::connection($dumpinto)->table('clients')->insert($clientData);
+                    DB::connection($dumpinto)->table('clients')->where('database_name',$client['database_name'])->update(['database_host' => $database_host_dev]);
+                  
+                    DB::disconnect($schemaName);
+                    return redirect()->route('client.index')->with('success', 'Client Migrated!');
+                } catch (Exception $ex) {
+                    return redirect()->route('client.index')->with('error', $ex->getMessage());
+                  
+                }
             }
             else{
                 return redirect()->route('client.index')->with('error', 'This client is already exist!!');
