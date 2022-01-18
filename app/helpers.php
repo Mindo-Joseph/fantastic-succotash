24e8066b8046b810799e035f10d23c031899aed7
Inderjit <inderjit.singh@codebrewinnovations.com>
2022-01-18 11:12:11 +0530

    done

--- app/helpers.php
+++ app/helpers.php
@@ -369,6 +369,7 @@ $viewSlot = array();
         }
     $arr = array();
     $count = count($slotss);
+    dd($count);
     if($count){
         for($i=0;$i<$count;$i++){
             $arr = array_merge($arr,$slotss[$i]);
