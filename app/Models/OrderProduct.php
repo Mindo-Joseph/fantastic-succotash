452c4ad64a3420789947e0bfe0d3b86aac31d9eb
dineshk123 <dineshk@codebrewinnovations.com>
2022-02-01 20:05:07 +0530

    merge

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -76,14 +76,12 @@ class OrderProduct extends Model{
       $image = $this->getImageAttribute($img);
       $image_url = $image['proxy_url'].'100/100'.$image['image_path'];
       try{
-        if(isset($image_url))
+        if(isset($image_url) && !empty($image_url))
         $base64 = base64_encode(file_get_contents($image_url));
         else
         $base64 = '';
         return $base64;
-  
-       
-      }
+     }
      catch(Exception $e) {
       $base64 = '';
       return $base64;
